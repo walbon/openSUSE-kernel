2f42cddd745251f1c54e934728a32c965ed8ffa0
Suresh Jayaraman <sjayaraman@suse.de>
2010-02-04 11:50:25 +0530

    - patches.fixes/sched-cpuacct-percpu-counter-batch.patch: sched:   cpuacct: Use bigger percpu counter batch values for stats   counters (bnc#575074).
    suse-commit: 17720621551c8175388edb7b163425eb1179b5b1

--- kernel/sched.c
+++ kernel/sched.c
@@ -10838,6 +10838,7 @@ static void cpuacct_update_stats(struct task_struct *tsk,
 		enum cpuacct_stat_index idx, cputime_t val)
 {
 	struct cpuacct *ca;
+	int batch;
 
 	if (unlikely(!cpuacct_subsys.active))
 		return;
@@ -10845,8 +10846,9 @@ static void cpuacct_update_stats(struct task_struct *tsk,
 	rcu_read_lock();
 	ca = task_ca(tsk);
 
+	batch = min_t(long, percpu_counter_batch * cputime_one_jiffy, INT_MAX);
 	do {
-		percpu_counter_add(&ca->cpustat[idx], val);
+		__percpu_counter_add(&ca->cpustat[idx], val, batch);
 		ca = ca->parent;
 	} while (ca);
 	rcu_read_unlock();
