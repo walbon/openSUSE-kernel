2f1a359dfaddab962b0aaf6d4278c3763f66b7a0
Nikanth Karthikesan <knikanth@suse.de>
2010-02-23 16:07:49 +0100

    - patches.fixes/bsg-SG_IO-compat_ioctl: compat_ioct: fix bsg SG_IO   (bnc#580991). - patches.fixes/fc_bsg_request_packed: fc-transport: Use   packed modifier for fc_bsg_request structure. (bnc#580991).
    suse-commit: 07a79d0b12afce3d04d5c5ce6c5d2afa559ed056

--- fs/compat_ioctl.c
+++ fs/compat_ioctl.c
@@ -736,6 +736,12 @@ static int sg_ioctl_trans(unsigned int fd, unsigned int cmd, unsigned long arg)
 	u32 data;
 	void __user *dxferp;
 	int err;
+	int interface_id;
+
+	if (get_user(interface_id, &sgio32->interface_id))
+		return -EFAULT;
+	if (interface_id != 'S')
+		return sys_ioctl(fd, cmd, (unsigned long)sgio32);
 
 	sgio32 = compat_ptr(arg);
 	if (get_user(iovec_count, &sgio32->iovec_count))
