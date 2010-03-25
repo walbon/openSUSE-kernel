3234c2325ae9668fcfe34a0d5966693201c53e12
Mark Fasheh <mfasheh@suse.com>
2010-03-25 15:34:42 -0700

    - patches.suse/ocfs2-allocation-resrvations.patch: Refresh.
    suse-commit: d80028441e6987d52ce9d1fad45864a58682dfa4

--- fs/ocfs2/super.c
+++ fs/ocfs2/super.c
@@ -178,8 +178,6 @@ enum {
 	Opt_usrquota,
 	Opt_grpquota,
 	Opt_resv_level,
-	Opt_dir_resv,
-	Opt_no_dir_resv,
 	Opt_err,
 };
 
@@ -207,8 +205,6 @@ static const match_table_t tokens = {
 	{Opt_usrquota, "usrquota"},
 	{Opt_grpquota, "grpquota"},
 	{Opt_resv_level, "resv_level=%u"},
-	{Opt_dir_resv, "dir_resv"},
-	{Opt_no_dir_resv, "no_dir_resv"},
 	{Opt_err, NULL}
 };
 
@@ -1450,12 +1446,6 @@ static int ocfs2_parse_options(struct super_block *sb,
 			    option < OCFS2_MAX_RESV_LEVEL)
 				mopt->resv_level = option;
 			break;
-		case Opt_dir_resv:
-			mopt->mount_opt |= OCFS2_MOUNT_DIR_RESV;
-			break;
-		case Opt_no_dir_resv:
-			mopt->mount_opt &= ~OCFS2_MOUNT_DIR_RESV;
-			break;
 		default:
 			mlog(ML_ERROR,
 			     "Unrecognized mount option \"%s\" "
@@ -1539,11 +1529,6 @@ static int ocfs2_show_options(struct seq_file *s, struct vfsmount *mnt)
 
 	if (osb->osb_resv_level != OCFS2_DEFAULT_RESV_LEVEL)
 		seq_printf(s, ",resv_level=%d", osb->osb_resv_level);
-
-	if (opts & OCFS2_MOUNT_DIR_RESV)
-		seq_printf(s, ",dir_resv");
-	else
-		seq_printf(s, ",no_dir_resv");
 
 	return 0;
 }
