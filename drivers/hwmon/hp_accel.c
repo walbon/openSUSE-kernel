ce7e6663cb9959b5eb5b75aa3a8ab037ca3d5792
Takashi Iwai <tiwai@suse.de>
2010-04-15 16:39:43 +0200

    - patches.drivers/lis3-add-support-for-hp-probook-2: lis3:   Add axis quirk for HP ProBook 532x (bnc#596943).
    suse-commit: 89c4915ebbe081e88293f2357c5f17cfc59fac03

--- drivers/hwmon/hp_accel.c
+++ drivers/hwmon/hp_accel.c
@@ -221,6 +221,7 @@ static struct dmi_system_id lis3lv02d_dmi_ids[] = {
 	AXIS_DMI_MATCH("HPB442x", "HP ProBook 442", xy_rotated_left),
 	AXIS_DMI_MATCH("HPB452x", "HP ProBook 452", y_inverted),
 	AXIS_DMI_MATCH("HPB522x", "HP ProBook 522", xy_swap),
+	AXIS_DMI_MATCH("HPB532x", "HP ProBook 532", y_inverted),
 	{ NULL, }
 /* Laptop models without axis info (yet):
  * "NC6910" "HP Compaq 6910"
