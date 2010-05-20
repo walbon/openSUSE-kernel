3a2ef9b1e2c5a50b688836398dd3003cfdf422c0
Jiri Kosina <jkosina@suse.cz>
2010-05-20 10:17:58 +0200

    - patches.fixes/ipv6-fix-dad-race.patch: Delete.   (fixes bnc#604183, partially fixes bnc#602969 and    reopens bnc#577967)
    Conflicts:
    
    	kernel-source.changes
    
    suse-commit: 9131c7ba2987acdf6a87c7a065c98b0253ca8456

--- net/ipv6/addrconf.c
+++ net/ipv6/addrconf.c
@@ -3974,7 +3974,8 @@ static void __ipv6_ifa_notify(int event, struct inet6_ifaddr *ifp)
 			addrconf_leave_anycast(ifp);
 		addrconf_leave_solict(ifp->idev, &ifp->addr);
 		dst_hold(&ifp->rt->u.dst);
-		ip6_del_rt(ifp->rt);
+		if (ip6_del_rt(ifp->rt))
+			dst_free(&ifp->rt->u.dst);
 		break;
 	}
 }
