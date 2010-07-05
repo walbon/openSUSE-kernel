372736ea5e2a7939f5b18ef0eebe53339b5517bb
Takashi Iwai <tiwai@suse.de>
2010-07-05 11:57:22 +0200

    - patches.drivers/alsa-sp1-hda-77-lenovo-thinkpad-x100e: ALSA:   hda - add support for Lenovo ThinkPad X100e in conexant codec   (bnc#619007).
    suse-commit: fa5d6f05dc6be9f88264ada29d47f64cb5884415

--- sound/pci/hda/patch_conexant.c
+++ sound/pci/hda/patch_conexant.c
@@ -2376,6 +2376,7 @@ static struct snd_pci_quirk cxt5066_cfg_tbl[] = {
 	SND_PCI_QUIRK(0x152d, 0x0833, "OLPC XO-1.5", CXT5066_OLPC_XO_1_5),
 	SND_PCI_QUIRK(0x1179, 0xff50, "Toshiba Satellite P500-PSPGSC-01800T", CXT5066_OLPC_XO_1_5),
 	SND_PCI_QUIRK(0x1179, 0xffe0, "Toshiba Satellite Pro T130-15F", CXT5066_OLPC_XO_1_5),
+	SND_PCI_QUIRK(0x17aa, 0x21b2, "Thinkpad X100e", CXT5066_OLPC_XO_1_5),
 	{}
 };
 
