65487b72156c4239875333e5b54375b1187c604b
Hannes Reinecke <hare@suse.de>
2010-07-27 08:48:47 +0200

    - patches.fixes/fcoe-remove-check-for-zero-fabric: fcoe: remove   check for zero fabric name (bnc#625666).
    suse-commit: b550a5f66a77b784e6daaf7b59b8dc338eb8ed9f

--- drivers/scsi/fcoe/libfcoe.c
+++ drivers/scsi/fcoe/libfcoe.c
@@ -714,7 +714,7 @@ static int fcoe_ctlr_parse_adv(struct fcoe_ctlr *fip,
 	}
 	if (!fcf->fc_map || (fcf->fc_map & 0x10000))
 		return -EINVAL;
-	if (!fcf->switch_name || !fcf->fabric_name)
+	if (!fcf->switch_name)
 		return -EINVAL;
 	return 0;
 
