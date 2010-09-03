05f06ffe5c0a70f4c532ce041c20e2fc876e30d3
Jeff Mahoney <jeffm@suse.com>
2010-09-03 12:33:42 -0400

    Revert "- patches.fixes/xfs-always-use-iget-in-bulkstat: xfs: always"
    This reverts commit 4ba23c92f4f0025d38e7c4257c7058b1ae5789a0.
    
    This was supposed to stay in my local repo.
    
    suse-commit: d4880576572efd83879473ea35c6cb7760dfff3c

--- fs/xfs/xfs_itable.h
+++ fs/xfs/xfs_itable.h
@@ -28,7 +28,9 @@ typedef int (*bulkstat_one_pf)(struct xfs_mount	*mp,
 			       void		__user *buffer,
 			       int		ubsize,
 			       void		*private_data,
+			       xfs_daddr_t	bno,
 			       int		*ubused,
+			       void		*dip,
 			       int		*stat);
 
 /*
@@ -37,6 +39,13 @@ typedef int (*bulkstat_one_pf)(struct xfs_mount	*mp,
 #define BULKSTAT_RV_NOTHING	0
 #define BULKSTAT_RV_DIDONE	1
 #define BULKSTAT_RV_GIVEUP	2
+
+/*
+ * Values for bulkstat flag argument.
+ */
+#define BULKSTAT_FG_IGET	0x1	/* Go through the buffer cache */
+#define BULKSTAT_FG_QUICK	0x2	/* No iget, walk the dinode cluster */
+#define BULKSTAT_FG_INLINE	0x4	/* No iget if inline attrs */
 
 /*
  * Return stat information in bulk (by-inode) for the filesystem.
@@ -50,6 +59,7 @@ xfs_bulkstat(
 	void		*private_data,	/* private data for formatter */
 	size_t		statstruct_size,/* sizeof struct that we're filling */
 	char		__user *ubuffer,/* buffer with inode stats */
+	int		flags,		/* flag to control access method */
 	int		*done);		/* 1 if there are more stats to get */
 
 int
@@ -72,7 +82,9 @@ xfs_bulkstat_one_int(
 	void			__user *buffer,
 	int			ubsize,
 	bulkstat_one_fmt_pf	formatter,
+	xfs_daddr_t		bno,
 	int			*ubused,
+	void			*dibuff,
 	int			*stat);
 
 int
@@ -82,7 +94,9 @@ xfs_bulkstat_one(
 	void			__user *buffer,
 	int			ubsize,
 	void			*private_data,
+	xfs_daddr_t		bno,
 	int			*ubused,
+	void			*dibuff,
 	int			*stat);
 
 int
--- fs/xfs/xfs_vnodeops.c
+++ fs/xfs/xfs_vnodeops.c
@@ -1366,7 +1366,7 @@ xfs_lookup(
 	if (error)
 		goto out;
 
-	error = xfs_iget(dp->i_mount, NULL, inum, 0, 0, ipp);
+	error = xfs_iget(dp->i_mount, NULL, inum, 0, 0, ipp, 0);
 	if (error)
 		goto out_free_name;
 
