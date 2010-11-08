dd8087eb6fc05b69dcd642bbfeceb12f1283265c
Suresh jayaraman <sjayaraman@suse.de>
2010-11-08 08:19:43 +0100

    - patches.suse/SoN-31-fix-kernel-bug-with-multiple-swapfiles:   fix mess up on swap with multi files from same nfs server   (FATE#303834). Fix kABI issues as well.
    suse-commit: 173d15a2babd2ae21d2aa478c378218e9ef5b258

--- include/linux/sunrpc/xprt.h
+++ include/linux/sunrpc/xprt.h
@@ -168,9 +168,16 @@ struct rpc_xprt {
 	unsigned int		max_reqs;	/* total slots */
 	unsigned long		state;		/* transport state */
 	unsigned char		shutdown   : 1,	/* being shut down */
-				resvport   : 1, /* use a reserved port */
-				swapper    : 1; /* we're swapping over this
+#ifndef __GENKSYMS__
+				resvport   : 1; /* use a reserved port */
+	unsigned int		swapper; 	/* we're swapping over this
 						   transport */
+#else
+				resvport   : 1, /* use a reserved port */
+				swapper	   : 1;	/* we're swapping over this
+						   transport */
+#endif
+
 	unsigned int		bind_index;	/* bind function index */
 
 	/*
