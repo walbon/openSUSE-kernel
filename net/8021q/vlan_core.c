7d5d37ec3190dbfa1601d9c5f07465f6522c3302
Jiri Bohac <jbohac@suse.cz>
2010-12-10 21:36:05 +0100

    - patches.fixes/bonding-allow-arp_ip_targers-on-separate-vlans-to-use-arp-validation.patch:   bonding: allow arp_ip_targets on separate vlans to use arp   validation (bnc#648701). - patches.fixes/bonding-set-device-in-rlb-arp-packet-handler.patch:   bonding: set device in RLB ARP packet handler (bnc#648701).
    suse-commit: ef8abb7515c46ea1c2ab054fc0e6c280190960ad

--- net/8021q/vlan_core.c
+++ net/8021q/vlan_core.c
@@ -14,6 +14,7 @@ int __vlan_hwaccel_rx(struct sk_buff *skb, struct vlan_group *grp,
 	if (skb_bond_should_drop(skb))
 		goto drop;
 
+	skb->iif = skb->dev->ifindex;
 	skb->vlan_tci = vlan_tci;
 	skb->dev = vlan_group_get_device(grp, vlan_tci & VLAN_VID_MASK);
 
@@ -82,6 +83,7 @@ static int vlan_gro_common(struct napi_struct *napi, struct vlan_group *grp,
 	if (skb_bond_should_drop(skb))
 		goto drop;
 
+	skb->iif = skb->dev->ifindex;
 	skb->vlan_tci = vlan_tci;
 	skb->dev = vlan_group_get_device(grp, vlan_tci & VLAN_VID_MASK);
 
