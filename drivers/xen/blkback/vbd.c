e6aeb156a4cfb961a8ba9216d40e2068e1c3ef67
Jan Beulich <jbeulich@novell.com>
2011-06-09 09:55:15 +0200

    - patches.xen/1089-blkback-barrier-check.patch: xen/blkback: don't fail   empty barrier requests. - patches.xen/1090-blktap-locking.patch: xen/blktap: fix locking   (bnc#685276). - patches.xen/1091-xenbus-dev-no-BUG.patch: xenbus: don't BUG() on user   mode induced conditions (bnc#696107). - Refresh other Xen patches.
    suse-commit: 87ec31d6cabe814a4097fa5b837a32d1b91786bb

--- drivers/xen/blkback/vbd.c
+++ drivers/xen/blkback/vbd.c
@@ -111,8 +111,14 @@ int vbd_translate(struct phys_req *req, blkif_t *blkif, int operation)
 	if (vbd->bdev == NULL)
 		goto out;
 
-	if (unlikely((req->sector_number + req->nr_sects) > vbd_sz(vbd)))
-		goto out;
+	if (likely(req->nr_sects)) {
+		blkif_sector_t end = req->sector_number + req->nr_sects;
+
+		if (unlikely(end < req->sector_number))
+			goto out;
+		if (unlikely(end > vbd_sz(vbd)))
+			goto out;
+	}
 
 	req->dev  = vbd->pdevice;
 	req->bdev = vbd->bdev;
