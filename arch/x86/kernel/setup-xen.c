b04c7bd05714af9f5b8c35a59466c764da0bfd96
Hannes Reinecke <hare@suse.de>
2011-06-14 12:30:07 +0200

    - patches.xen/xen-change-reserve_ibft_region-to-find_ibft.patch:   Change reserve_ibft_region() to find_ibft_region()   (FATE#311488).
    suse-commit: b80b63a6e9f61a6404d6b1d3954a4881c5863d68

--- arch/x86/kernel/setup-xen.c
+++ arch/x86/kernel/setup-xen.c
@@ -693,6 +693,16 @@ static int __init setup_elfcorehdr(char *arg)
 early_param("elfcorehdr", setup_elfcorehdr);
 #endif
 
+static __init void reserve_ibft_region(void)
+{
+	unsigned long addr, size = 0;
+
+	addr = find_ibft_region(&size);
+
+	if (size)
+		reserve_early_overlap_ok(addr, addr + size, "ibft");
+}
+
 #ifdef CONFIG_X86_RESERVE_LOW_64K
 static int __init dmi_low_memory_corruption(const struct dmi_system_id *d)
 {
