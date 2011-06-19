dea0b274831dbcd2f4e219894d011c029473d89a
Jean Delvare <jdelvare@suse.de>
2011-06-19 20:37:50 +0200

    - patches.drivers/hwmon-coretemp-add-pkgtemp-support.patch:   Add package level thermal control in coretemp (FATE#311941,   bnc#698797).
    suse-commit: 2c463780ac1394bf58f1b3681e9f6bc6c9639c0a

--- arch/x86/include/asm/cpufeature.h
+++ arch/x86/include/asm/cpufeature.h
@@ -162,6 +162,8 @@
  */
 #define X86_FEATURE_IDA		(7*32+ 0) /* Intel Dynamic Acceleration */
 #define X86_FEATURE_ARAT	(7*32+ 1) /* Always Running APIC Timer */
+#define X86_FEATURE_PTS		(7*32+ 6) /* Intel Package Thermal Status */
+#define X86_FEATURE_DTS		(7*32+ 7) /* Digital Thermal Sensor */
 
 /* Virtualization flags: Linux defined */
 #define X86_FEATURE_TPR_SHADOW  (8*32+ 0) /* Intel TPR Shadow */
