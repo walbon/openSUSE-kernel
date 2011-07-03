9cf1690b5a75ffce323fec8b53e97604b7d8d299
Jean Delvare <jdelvare@suse.de>
2011-07-03 23:17:57 +0200

    - patches.arch/x86-enable-package-level-power-notification.patch:   Package level power notification (FATE#311941, bnc#698801). - patches.arch/0001-x86-cpu-Add-AMD-core-boosting-feature-flag-to-proc-c.patch:   Refresh. - patches.drivers/edac_do_not_taint_on_correctable_errors.patch:   Refresh. - patches.suse/revert-x86-under-bios-control-restore-ap-s-apic_lvtthmr-to-the-bsp-value:   Refresh. - patches.suse/x86-under-bios-control-restore-ap-s-apic_lvtthmr-to-the-bsp-value:   Refresh.
    suse-commit: 341a1ea1521c08aaf859b0f3a875d82659c9fa3e

--- arch/x86/include/asm/cpufeature.h
+++ arch/x86/include/asm/cpufeature.h
@@ -169,6 +169,7 @@
 #define X86_FEATURE_CPB		(7*32+ 2) /* AMD Core Performance Boost */
 #define X86_FEATURE_EPB		(7*32+ 3) /* IA32_ENERGY_PERF_BIAS support */
 #define X86_FEATURE_XSAVEOPT	(7*32+ 4) /* "xsaveopt" Optimized Xsave */
+#define X86_FEATURE_PLN		(7*32+ 5) /* Intel Power Limit Notification */
 #define X86_FEATURE_PTS		(7*32+ 6) /* Intel Package Thermal Status */
 #define X86_FEATURE_DTS		(7*32+ 7) /* Digital Thermal Sensor */
 
--- arch/x86/kernel/cpu/addon_cpuid_features.c
+++ arch/x86/kernel/cpu/addon_cpuid_features.c
@@ -34,6 +34,7 @@ void __cpuinit init_scattered_cpuid_features(struct cpuinfo_x86 *c)
 		{ X86_FEATURE_DTS,		CR_EAX, 0, 0x00000006, 0 },
  		{ X86_FEATURE_IDA,		CR_EAX, 1, 0x00000006, 0 },
 		{ X86_FEATURE_ARAT,		CR_EAX, 2, 0x00000006, 0 },
+		{ X86_FEATURE_PLN,		CR_EAX, 4, 0x00000006, 0 },
 		{ X86_FEATURE_PTS,		CR_EAX, 6, 0x00000006, 0 },
 		{ X86_FEATURE_EPB,		CR_ECX, 3, 0x00000006, 0 },
  		{ X86_FEATURE_XSAVEOPT,		CR_EAX,	0, 0x0000000d, 1 },
