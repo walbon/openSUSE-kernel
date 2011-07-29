8a818616c6a81870552f452bdc00a5adaf3db696
Michal Marek <mmarek@suse.cz>
2011-07-29 17:42:51 +0200

    - patches.drivers/0166-bnx2x-Look-inside-vlan-when-determining-checksum-pro.patch:   Refresh. - patches.suse/vlan-Add-function-to-retrieve-EtherType-from-vlan-packets:   vlan: Add function to retrieve EtherType from vlan packets   (bnc#691742, fate#311957).
    suse-commit: 9bdfbf1a38c9800f1cfc623bd32d93fa2d3f52de

--- drivers/net/bnx2x/bnx2x_compat.h
+++ drivers/net/bnx2x/bnx2x_compat.h
@@ -769,36 +769,6 @@ static inline int netif_set_real_num_rx_queues(struct net_device *dev, int num)
 }
 #endif
 
-/**
- * vlan_get_protocol - get protocol EtherType.
- * @skb: skbuff to query
- *
- * Returns the EtherType of the packet, regardless of whether it is
- * vlan encapsulated (normal or hardware accelerated) or not.
- */
-static inline __be16 vlan_get_protocol(const struct sk_buff *skb)
-{
-	__be16 protocol = 0;
-
-#ifdef BCM_VLAN
-	if (vlan_tx_tag_present(skb) ||
-	     skb->protocol != cpu_to_be16(ETH_P_8021Q))
-#else
-	if (skb->protocol != cpu_to_be16(ETH_P_8021Q))
-#endif
-		protocol = skb->protocol;
-	else {
-		__be16 proto, *protop;
-		protop = skb_header_pointer(skb, offsetof(struct vlan_ethhdr,
-						h_vlan_encapsulated_proto),
-						sizeof(proto), &proto);
-		if (likely(protop))
-			protocol = *protop;
-	}
-
-	return protocol;
-}
-
 #ifndef __rcu
 #define __rcu
 #endif
