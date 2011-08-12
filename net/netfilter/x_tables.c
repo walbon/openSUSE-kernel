fe0efe562273a797173a5952c42b9bc6ee9f072b
Jiri Bohac <jbohac@suse.cz>
2011-08-12 17:19:54 +0200

    - Update config files. - patches.suse/audit-export-symbol-for-use-with-xt_AUDIT.patch:   audit: export symbol for use with xt_AUDIT (fate#312390,   fate#312386). - patches.suse/netfilter-audit-target.patch: netfilter: audit   target to record accepted/dropped packets (fate#312390,   fate#312386). - patches.suse/netfilter-create-audit-records-for-x_tables-replaces.patch:   netfilter: create audit records for x_tables replaces   (fate#312390, fate#312386). - patches.suse/netfilter-ebtables-fix-xt_AUDIT-to-work-with-ebtables.patch:   netfilter ebtables: fix xt_AUDIT to work with ebtables. - supported.conf:
    suse-commit: 50fcb6ba214020a0f5995769ce9380a343edc170

--- net/netfilter/x_tables.c
+++ net/netfilter/x_tables.c
@@ -22,6 +22,7 @@
 #include <linux/vmalloc.h>
 #include <linux/mutex.h>
 #include <linux/mm.h>
+#include <linux/audit.h>
 #include <net/net_namespace.h>
 
 #include <linux/netfilter/x_tables.h>
@@ -731,6 +732,21 @@ xt_replace_table(struct xt_table *table,
 	 * during the get_counters() routine.
 	 */
 	local_bh_enable();
+
+#ifdef CONFIG_AUDIT
+	if (audit_enabled) {
+		struct audit_buffer *ab;
+
+		ab = audit_log_start(current->audit_context, GFP_KERNEL,
+				     AUDIT_NETFILTER_CFG);
+		if (ab) {
+			audit_log_format(ab, "table=%s family=%u entries=%u",
+					 table->name, table->af,
+					 private->number);
+			audit_log_end(ab);
+		}
+	}
+#endif
 
 	return private;
 }
