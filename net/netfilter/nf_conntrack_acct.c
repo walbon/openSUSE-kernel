3408f6d46d96f6a797e784bb145cd32e063b3cb2
Jiri Bohac <jbohac@suse.cz>
2011-08-23 14:01:19 +0200

    - patches.suse/enable_nf_conntrack_accounting_by_default.patch:   enable nf_conntrack accounting by default. - patches.fixes/netfilter-remove-pointless-config_nf_ct_acct-warning:   Delete.
    suse-commit: 91da8fa71866eacb141d85f80c6dbb8bc0624846

--- net/netfilter/nf_conntrack_acct.c
+++ net/netfilter/nf_conntrack_acct.c
@@ -17,7 +17,7 @@
 #include <net/netfilter/nf_conntrack_extend.h>
 #include <net/netfilter/nf_conntrack_acct.h>
 
-static int nf_ct_acct __read_mostly;
+static int nf_ct_acct __read_mostly = 1;
 
 module_param_named(acct, nf_ct_acct, bool, 0644);
 MODULE_PARM_DESC(acct, "Enable connection tracking flow accounting.");
