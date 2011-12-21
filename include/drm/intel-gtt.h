4bfcda380a2863bae98a9958483f1fc02433d1dd
Takashi Iwai <tiwai@suse.de>
2011-12-21 12:30:25 +0100

    - patches.kabi/drm-i915-ILK-VT-d-workaround-kABI-fix: Fix kABI   for patches.drivers/drm-i915-ILK-VT-d-workaround.
    suse-commit: 1ddb90a147367b03aa86fb283484e60b0afa4b16

--- include/drm/intel-gtt.h
+++ include/drm/intel-gtt.h
@@ -13,8 +13,10 @@ const struct intel_gtt {
 	unsigned int gtt_mappable_entries;
 	/* Whether i915 needs to use the dmar apis or not. */
 	unsigned int needs_dmar : 1;
+#ifndef __GENKSYMS__
 	/* Whether we idle the gpu before mapping/unmapping */
 	unsigned int do_idle_maps : 1;
+#endif
 } *intel_gtt_get(void);
 
 void intel_gtt_chipset_flush(void);
