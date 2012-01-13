a9bfbac562c2c6d9cb5bfb71cbf64bb5262319c4
David Sterba <dsterba@suse.cz>
2012-01-13 14:25:17 +0100

    - patches.suse/btrfs-8024-workaround-for-cleaner-deadlock.patch:   enhance the workaround
    suse-commit: 2b38f3996c768b9d64bad9e37a0641ecb85ebe55

--- fs/btrfs/extent-tree.c
+++ fs/btrfs/extent-tree.c
@@ -3509,7 +3509,9 @@ static int shrink_delalloc(struct btrfs_root *root, u64 to_reclaim,
 			 max_reclaim >> PAGE_CACHE_SHIFT);
 	while (loops < 1024) {
 		/* have the flusher threads jump in and do some IO */
-		smp_mb();
+		if (btrfs_fs_closing(root->fs_info))
+			return -EAGAIN;
+
 		nr_pages = min_t(unsigned long, nr_pages,
 		       root->fs_info->delalloc_bytes >> PAGE_CACHE_SHIFT);
 		writeback_inodes_sb_nr_if_idle(root->fs_info->sb, nr_pages);
