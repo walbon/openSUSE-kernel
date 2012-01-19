97853a40d224c1dcc4a7ad24ba8d4a08a445708b
Jeff Mahoney <jeffm@suse.com>
2012-01-19 11:27:00 -0500

    - patches.drivers/revert-libata-implement-on-demand-hpa-unlocking:   revert: libata: implement on-demand HPA unlocking (bnc#713675).
    suse-commit: 9e10e7112bfa5133ade55231e872f65306802446

--- include/linux/libata.h
+++ include/linux/libata.h
@@ -1037,7 +1037,6 @@ extern void ata_scsi_simulate(struct ata_device *dev, struct scsi_cmnd *cmd);
 extern int ata_std_bios_param(struct scsi_device *sdev,
 			      struct block_device *bdev,
 			      sector_t capacity, int geom[]);
-extern void ata_scsi_unlock_native_capacity(struct scsi_device *sdev);
 extern int ata_scsi_slave_config(struct scsi_device *sdev);
 extern void ata_scsi_slave_destroy(struct scsi_device *sdev);
 extern int ata_scsi_change_queue_depth(struct scsi_device *sdev,
@@ -1194,7 +1193,6 @@ extern struct device_attribute *ata_common_sdev_attrs[];
 	.slave_configure	= ata_scsi_slave_config,	\
 	.slave_destroy		= ata_scsi_slave_destroy,	\
 	.bios_param		= ata_std_bios_param,		\
-	.unlock_native_capacity	= ata_scsi_unlock_native_capacity, \
 	.sdev_attrs		= ata_common_sdev_attrs
 
 #define ATA_NCQ_SHT(drv_name)					\
