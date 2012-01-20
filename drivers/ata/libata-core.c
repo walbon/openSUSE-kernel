5fad42dd5ad88dc19502758e71c958fe27f3c1fb
Michal Marek <mmarek@suse.cz>
2012-01-20 13:56:26 +0100

    - patches.kabi/ata_scsi_unlock_native_capacity-stub: kabi:   Provide stub for ata_scsi_unlock_native_capacity().
    suse-commit: 41ffc5cb016e8a8ae052f7f705de5f62879b8d8b

--- drivers/ata/libata-core.c
+++ drivers/ata/libata-core.c
@@ -6716,3 +6716,10 @@ EXPORT_SYMBOL_GPL(ata_cable_80wire);
 EXPORT_SYMBOL_GPL(ata_cable_unknown);
 EXPORT_SYMBOL_GPL(ata_cable_ignore);
 EXPORT_SYMBOL_GPL(ata_cable_sata);
+
+
+/* Stub for kabi compatibility */
+void ata_scsi_unlock_native_capacity(struct scsi_device *sdev)
+{
+}
+EXPORT_SYMBOL_GPL(ata_scsi_unlock_native_capacity); 
