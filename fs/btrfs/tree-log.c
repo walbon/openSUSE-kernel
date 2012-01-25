ba8f3422ccd71aca1ce0bdb4dcd0eca660ab7e5b
Jan Kara <jack@suse.cz>
2012-01-25 15:08:32 +0100

    - patches.suse/btrfs-Fix-busyloops-in-transaction-waiting-code.patch:   btrfs: Fix busyloops in transaction waiting code (bnc#730103).
    suse-commit: 286b21068a5659a24ba7e0f59ab2801dbe121ac8

--- fs/btrfs/tree-log.c
+++ fs/btrfs/tree-log.c
@@ -1962,7 +1962,8 @@ static int wait_log_commit(struct btrfs_trans_handle *trans,
 
 		finish_wait(&root->log_commit_wait[index], &wait);
 		mutex_lock(&root->log_mutex);
-	} while (root->log_transid < transid + 2 &&
+	} while (root->fs_info->last_trans_log_full_commit !=
+		 trans->transid && root->log_transid < transid + 2 &&
 		 atomic_read(&root->log_commit[index]));
 	return 0;
 }
@@ -1971,7 +1972,8 @@ static void wait_for_writer(struct btrfs_trans_handle *trans,
 			    struct btrfs_root *root)
 {
 	DEFINE_WAIT(wait);
-	while (atomic_read(&root->log_writers)) {
+	while (root->fs_info->last_trans_log_full_commit !=
+	       trans->transid && atomic_read(&root->log_writers)) {
 		prepare_to_wait(&root->log_writer_wait,
 				&wait, TASK_UNINTERRUPTIBLE);
 		mutex_unlock(&root->log_mutex);
