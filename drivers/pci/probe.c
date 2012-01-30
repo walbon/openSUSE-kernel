5ecd947d14b0796ab379d950d9d8d16b74cbc90b
Thomas Renninger <trenn@suse.de>
2012-01-30 11:35:09 +0100

    - patches.fixes/bug-735909_hard_wired_primary_bus.patch:   workaround hard-wired bus number (bnc#735909).
    suse-commit: fad2934d2299f09540ef10876a3cceff45034dea

--- drivers/pci/probe.c
+++ drivers/pci/probe.c
@@ -657,6 +657,10 @@ int __devinit pci_scan_bridge(struct pci_bus *bus, struct pci_dev *dev, int max,
 	dev_dbg(&dev->dev, "scanning [bus %02x-%02x] behind bridge, pass %d\n",
 		secondary, subordinate, pass);
 
+	/* some bridge primary bus is hard wired to 0 */
+	if (!primary && (primary != bus->number) && secondary && subordinate)
+		primary = bus->number;
+
 	/* Check if setup is sensible at all */
 	if (!pass &&
 	    (primary != bus->number || secondary <= bus->number)) {
