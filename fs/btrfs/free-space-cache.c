2acaf0c994825b9fe1dec0976bbb1de3d88bedf5
David Sterba <dsterba@suse.cz>
2012-02-16 17:03:42 +0100

    btrfs: enable block allocator fixes again - patches.suse/btrfs-btrfs_reserve_extent-check-sinfo: Refresh. - patches.suse/btrfs-handle-EIO: Refresh.
    suse-commit: 3d5153db1254a45d0742c77c0aaf8c4d696eb5bc

--- fs/btrfs/free-space-cache.c
+++ fs/btrfs/free-space-cache.c
@@ -2246,7 +2246,7 @@ u64 btrfs_alloc_from_cluster(struct btrfs_block_group_cache *block_group,
 		if (entry->bitmap) {
 			ret = btrfs_alloc_from_bitmap(block_group,
 						      cluster, entry, bytes,
-						      min_start);
+						      cluster->window_start);
 			if (ret == 0) {
 				node = rb_next(&entry->offset_index);
 				if (!node)
@@ -2255,6 +2255,7 @@ u64 btrfs_alloc_from_cluster(struct btrfs_block_group_cache *block_group,
 						 offset_index);
 				continue;
 			}
+			cluster->window_start += bytes;
 		} else {
 			ret = entry->offset;
 
@@ -2479,7 +2480,7 @@ setup_cluster_bitmap(struct btrfs_block_group_cache *block_group,
 	}
 
 	list_for_each_entry(entry, bitmaps, list) {
-		if (entry->bytes < min_bytes)
+		if (entry->bytes < bytes)
 			continue;
 		ret = btrfs_bitmap_cluster(block_group, entry, cluster, offset,
 					   bytes, cont1_bytes, min_bytes);
