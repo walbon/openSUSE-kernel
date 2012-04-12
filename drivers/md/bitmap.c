5a6de86d1ca6eb5214939e2a7b1b72f4754f215a
NeilBrown <neilb@suse.de>
2012-04-12 14:12:20 +1000

    - patches.fixes/md-bitmap-hot-add.fix: Delete. Equivalent   patch came in with 3.0.27
    suse-commit: 8d031ef6a9a404ebaa0c984efdd6555317599601

--- drivers/md/bitmap.c
+++ drivers/md/bitmap.c
@@ -1987,8 +1987,7 @@ location_store(mddev_t *mddev, const char *buf, size_t len)
 				if (rv) {
 					bitmap_destroy(mddev);
 					mddev->bitmap_info.offset = 0;
-				} else
-					bitmap_load(mddev);
+				}
 				mddev->pers->quiesce(mddev, 0);
 				if (rv)
 					return rv;
