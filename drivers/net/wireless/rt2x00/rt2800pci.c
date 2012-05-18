ba199be572b0d50e9ce2da84d64eccf29e9be91a
Michal Marek <mmarek@suse.cz>
2012-05-18 23:41:19 +0200

    - patches.drivers/rt2x00-add-RT539b: rt2x00:Add RT539b chipset   support (bnc#760237).
    suse-commit: 8b4f9ed1ecb3b74a9ed8e9ee5223e4a77ec8daef

--- drivers/net/wireless/rt2x00/rt2800pci.c
+++ drivers/net/wireless/rt2x00/rt2800pci.c
@@ -1146,6 +1146,7 @@ static DEFINE_PCI_DEVICE_TABLE(rt2800pci_device_table) = {
 #ifdef CONFIG_RT2800PCI_RT53XX
 	{ PCI_DEVICE(0x1814, 0x5390) },
 	{ PCI_DEVICE(0x1814, 0x539a) },
+	{ PCI_DEVICE(0x1814, 0x539b) },
 	{ PCI_DEVICE(0x1814, 0x539f) },
 #endif
 	{ 0, }
