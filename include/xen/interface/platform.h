16520d1d59d99e43c22e8378dece0c96f150644e
Jan Beulich <jbeulich@novell.com>
2012-06-22 10:43:36 +0200

    - Update Xen patches to 3.0.35. - patches.xen/frontswap-cleancache-invalidate,   patches.xen/frontswap-config, patches.xen/frontswap-core,   patches.xen/frontswap-header, patches.xen/frontswap-hooks:   Update mainline tags. - Refresh other Xen patches. - patches.xen/xen3-thp-avoid-atomic64_read-in-pmd_read_atomic-for-32-bit-PAE.patch:   thp: avoid atomic64_read in pmd_read_atomic for 32bit PAE   (bnc#762991). - Update Xen config files (CONFIG_XEN_SPINLOCK_ACQUIRE_NESTING=1).
    suse-commit: 55f8d94214204ba341bf189b9f4c443fbb643c98

--- include/xen/interface/platform.h
+++ include/xen/interface/platform.h
@@ -490,6 +490,20 @@ struct xenpf_mem_hotadd
     uint32_t flags;
 };
 
+#define XENPF_core_parking  60
+
+#define XEN_CORE_PARKING_SET 1
+#define XEN_CORE_PARKING_GET 2
+struct xenpf_core_parking {
+    /* IN variables */
+    uint32_t type;
+    /* IN variables:  set cpu nums expected to be idled */
+    /* OUT variables: get cpu nums actually be idled */
+    uint32_t idle_nums;
+};
+typedef struct xenpf_core_parking xenpf_core_parking_t;
+DEFINE_XEN_GUEST_HANDLE(xenpf_core_parking_t);
+
 #define XENPF_get_cpu_freq        ('N' << 24)
 #define XENPF_get_cpu_freq_min    (XENPF_get_cpu_freq + 1)
 #define XENPF_get_cpu_freq_max    (XENPF_get_cpu_freq_min + 1)
@@ -521,6 +535,7 @@ struct xen_platform_op {
         struct xenpf_cpu_ol            cpu_ol;
         struct xenpf_cpu_hotadd        cpu_add;
         struct xenpf_mem_hotadd        mem_add;
+        struct xenpf_core_parking      core_parking;
         struct xenpf_get_cpu_freq      get_cpu_freq;
         uint8_t                        pad[128];
     } u;
