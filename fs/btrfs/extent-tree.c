0c8bb45477f30fa7f5074ad2b9ba8e80c177acdc
David Sterba <dsterba@suse.cz>
2012-08-16 01:50:02 +0200

    - disable patch due to excessive warnings   (patches.suse/btrfs-8133-fix-race-in-run_clustered_refs.patch)
    suse-commit: 9e61186865f169fc0f45aa23162cf7c0f98bf1f8

--- fs/btrfs/extent-tree.c
+++ fs/btrfs/extent-tree.c
@@ -2317,23 +2317,6 @@ static noinline int run_clustered_refs(struct btrfs_trans_handle *trans,
 		ref->in_tree = 0;
 		rb_erase(&ref->rb_node, &delayed_refs->root);
 		delayed_refs->num_entries--;
-		if (locked_ref) {
-			/*
-			 * when we play the delayed ref, also correct the
-			 * ref_mod on head
-			 */
-			switch (ref->action) {
-			case BTRFS_ADD_DELAYED_REF:
-			case BTRFS_ADD_DELAYED_EXTENT:
-				locked_ref->node.ref_mod -= ref->ref_mod;
-				break;
-			case BTRFS_DROP_DELAYED_REF:
-				locked_ref->node.ref_mod += ref->ref_mod;
-				break;
-			default:
-				WARN_ON(1);
-			}
-		}
 		/*
 		 * we modified num_entries, but as we're currently running
 		 * delayed refs, skip
