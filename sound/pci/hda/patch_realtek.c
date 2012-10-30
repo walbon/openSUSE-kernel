f31ac0bfe369a79f8f31267d9d814556c95547e9
Takashi Iwai <tiwai@suse.de>
2012-10-30 17:34:55 +0100

    ALSA: hda: Virtualize SPDIF out controls (FATE#314310).
    suse-commit: 723248acd00f359d4e8168542a9683ea36f39b26

--- sound/pci/hda/patch_realtek.c
+++ sound/pci/hda/patch_realtek.c
@@ -3247,6 +3247,7 @@ static int alc_build_controls(struct hda_codec *codec)
 	}
 	if (spec->multiout.dig_out_nid) {
 		err = snd_hda_create_spdif_out_ctls(codec,
+						    spec->multiout.dig_out_nid,
 						    spec->multiout.dig_out_nid);
 		if (err < 0)
 			return err;
