267f85b2affeb05a0d259725ff638645d83443ac
Thomas Renninger <trenn@suse.de>
2012-11-02 18:21:05 +0100

    - patches.arch/x86_powernow-k8-dont-complain-on-missing-cpufreq-on-hp.patch:   X86 cpufreq: Do not complain on missing cpufreq tables on   ProLiants (bnc#787202).
    suse-commit: a717338f22365ad4bee3d67e69360ad35a301b24

--- drivers/cpufreq/powernow-k8.c
+++ drivers/cpufreq/powernow-k8.c
@@ -34,6 +34,7 @@
 #include <linux/cpumask.h>
 #include <linux/io.h>
 #include <linux/delay.h>
+#include <linux/dmi.h>
 
 #include <asm/msr.h>
 
@@ -1256,6 +1257,14 @@ static void __cpuinit powernowk8_cpu_init_on_cpu(void *_init_on_cpu)
 	init_on_cpu->rc = 0;
 }
 
+/*
+ * Latest HP ProLiant do cpufreq in BIOS by default and may not present
+ * P-state related ACPI information to the OS/driver on cpufreq capable
+ * machines on purpose.
+ * Do not throw a firmware bug exception for these.
+ */
+static int is_proliant = -1;
+
 /* per CPU init entry point to the driver */
 static int __cpuinit powernowk8_cpu_init(struct cpufreq_policy *pol)
 {
@@ -1266,6 +1275,9 @@ static int __cpuinit powernowk8_cpu_init(struct cpufreq_policy *pol)
 	struct init_on_cpu init_on_cpu;
 	int rc;
 	struct cpuinfo_x86 *c = &cpu_data(pol->cpu);
+
+	if (is_proliant == -1)
+		is_proliant = dmi_name_in_vendors("ProLiant");
 
 	if (!cpu_online(pol->cpu))
 		return -ENODEV;
@@ -1289,7 +1301,11 @@ static int __cpuinit powernowk8_cpu_init(struct cpufreq_policy *pol)
 		 * an UP version, and is deprecated by AMD.
 		 */
 		if (num_online_cpus() != 1) {
-			printk_once(ACPI_PSS_BIOS_BUG_MSG);
+			if (is_proliant)
+				printk_once(KERN_INFO FW_INFO PFX
+    "HP ProLiant without cpufreq info. Assume BIOS driven cpufreq scaling\n");
+			else
+				printk_once(ACPI_PSS_BIOS_BUG_MSG);
 			goto err_out;
 		}
 		if (pol->cpu != 0) {
