38de7a1485da5d8717d3266ed6218cca8190b5b7
Lee, Chun-Yi <jlee@suse.com>
2012-11-14 15:40:12 +0800

    efi.h: Add boottime->locate_handle search types.
    suse-commit: 5f8405bc0375d09a4d423516cd3bf7403fb25e30

--- include/linux/efi.h
+++ include/linux/efi.h
@@ -460,6 +460,13 @@ extern int __init efi_setup_pcdp_console(char *);
 				EFI_VARIABLE_TIME_BASED_AUTHENTICATED_WRITE_ACCESS | \
 				EFI_VARIABLE_APPEND_WRITE)
 /*
+ * The type of search to perform when calling boottime->locate_handle
+ */
+#define EFI_LOCATE_ALL_HANDLES			0
+#define EFI_LOCATE_BY_REGISTER_NOTIFY		1
+#define EFI_LOCATE_BY_PROTOCOL			2
+
+/*
  * EFI Device Path information
  */
 #define EFI_DEV_HW			0x01
