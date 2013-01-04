848d8ce51e2cb2e03f4cc97b898c5f03880d6cbe
Torsten Duwe <duwe@suse.de>
2013-01-04 17:30:59 +0100

    powerpc/pseries/cpuidle: Replace pseries_notify_cpuidle_add call with notifier (fate#314019,bnc#795230).
    suse-commit: 603cd0d0934ea90ed75d48c24e623ffdea6023ba

--- arch/powerpc/include/asm/system.h
+++ arch/powerpc/include/asm/system.h
@@ -226,10 +226,8 @@ void cpu_idle_wait(void);
 
 #ifdef CONFIG_PSERIES_IDLE
 extern void update_smt_snooze_delay(int snooze);
-extern int pseries_notify_cpuidle_add_cpu(int cpu);
 #else
 static inline void update_smt_snooze_delay(int snooze) {}
-static inline int pseries_notify_cpuidle_add_cpu(int cpu) { return 0; }
 #endif
 
 /*
