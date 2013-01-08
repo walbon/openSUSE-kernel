42c4c40c8a17fca6471c24e9137809b06c0b602d
Benjamin Poirier <bpoirier@suse.de>
2013-01-08 14:59:31 -0500

    be2net: Fix configuring VLAN for VF for Lancer (bnc#777565 FATE#313819).
    suse-commit: 70363125bc967ea5a89e48741bb78075bb28bfb4

--- drivers/net/ethernet/emulex/benet/be_main.c
+++ drivers/net/ethernet/emulex/benet/be_main.c
@@ -879,7 +879,12 @@ static void be_vlan_add_vid(struct net_device *netdev, u16 vid)
 	struct be_adapter *adapter = netdev_priv(netdev);
 
 	adapter->vlans_added++;
-	if (!be_physfn(adapter))
+
+	if (!lancer_chip(adapter) && !be_physfn(adapter))
+		return;
+
+	/* Packets with VID 0 are always received by Lancer by default */
+	if (lancer_chip(adapter) && vid == 0)
 		return;
 
 	adapter->vlan_tag[vid] = 1;
@@ -893,7 +898,11 @@ static void be_vlan_rem_vid(struct net_device *netdev, u16 vid)
 
 	adapter->vlans_added--;
 
-	if (!be_physfn(adapter))
+	if (!lancer_chip(adapter) && !be_physfn(adapter))
+		return;
+
+	/* Packets with VID 0 are always received by Lancer by default */
+	if (lancer_chip(adapter) && vid == 0)
 		return;
 
 	adapter->vlan_tag[vid] = 0;
