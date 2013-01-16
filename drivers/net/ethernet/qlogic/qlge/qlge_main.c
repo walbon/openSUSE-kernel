202ce730bc8d1722d370c15bbc50d2c34b0a541f
Benjamin Poirier <bpoirier@suse.de>
2013-01-16 16:30:51 -0500

    qlge: remove unnecessary setting of skb->dev (bnc#784811 FATE#313897).
    suse-commit: 1327204d361c384700dab40e4f29f9fbae411446

--- drivers/net/ethernet/qlogic/qlge/qlge_main.c
+++ drivers/net/ethernet/qlogic/qlge/qlge_main.c
@@ -1618,7 +1618,6 @@ static void ql_process_mac_rx_skb(struct ql_adapter *qdev,
 	}
 
 	prefetch(skb->data);
-	skb->dev = ndev;
 	if (ib_mac_rsp->flags1 & IB_MAC_IOCB_RSP_M_MASK) {
 		netif_printk(qdev, rx_status, KERN_DEBUG, qdev->ndev,
 			     "%s Multicast.\n",
@@ -1933,7 +1932,6 @@ static void ql_process_mac_split_rx_intr(struct ql_adapter *qdev,
 	}
 
 	prefetch(skb->data);
-	skb->dev = ndev;
 	if (ib_mac_rsp->flags1 & IB_MAC_IOCB_RSP_M_MASK) {
 		netif_printk(qdev, rx_status, KERN_DEBUG, qdev->ndev, "%s Multicast.\n",
 			     (ib_mac_rsp->flags1 & IB_MAC_IOCB_RSP_M_MASK) ==
