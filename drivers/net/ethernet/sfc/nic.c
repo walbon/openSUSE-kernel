badfc057da8f6da2bc8b29859bec304b8f7d8b89
Benjamin Poirier <bpoirier@suse.de>
2013-01-17 14:50:16 -0500

    sfc: Disable flow control during flushes (bnc#786035 FATE#314299).
    suse-commit: cc8f6ec57d20b124f386d02dd136ca8a0b5e95e3

--- drivers/net/ethernet/sfc/nic.c
+++ drivers/net/ethernet/sfc/nic.c
@@ -685,6 +685,7 @@ int efx_nic_flush_queues(struct efx_nic *efx)
 	struct efx_tx_queue *tx_queue;
 	int rc = 0;
 
+	efx->fc_disable++;
 	efx->type->prepare_flush(efx);
 
 	efx_for_each_channel(channel, efx) {
@@ -734,6 +735,8 @@ int efx_nic_flush_queues(struct efx_nic *efx)
 		atomic_set(&efx->rxq_flush_pending, 0);
 		atomic_set(&efx->rxq_flush_outstanding, 0);
 	}
+
+	efx->fc_disable--;
 
 	return rc;
 }
