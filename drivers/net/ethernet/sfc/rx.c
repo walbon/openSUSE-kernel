d9b97f457bca5cf0e6acc2d9eef605d6cb7b9c73
Benjamin Poirier <bpoirier@suse.de>
2013-01-17 14:50:16 -0500

    sfc: Leave interrupts and event queues enabled whenever we can (bnc#786035 FATE#314299).
    suse-commit: fd6b470d33c1980d34b6cefc18124437ef39004d

--- drivers/net/ethernet/sfc/rx.c
+++ drivers/net/ethernet/sfc/rx.c
@@ -704,6 +704,7 @@ void efx_init_rx_queue(struct efx_rx_queue *rx_queue)
 	rx_queue->fast_fill_limit = limit;
 
 	/* Set up RX descriptor ring */
+	rx_queue->enabled = true;
 	efx_nic_init_rx(rx_queue);
 }
 
@@ -714,6 +715,9 @@ void efx_fini_rx_queue(struct efx_rx_queue *rx_queue)
 
 	netif_dbg(rx_queue->efx, drv, rx_queue->efx->net_dev,
 		  "shutting down RX queue %d\n", efx_rx_queue_index(rx_queue));
+
+	/* A flush failure might have left rx_queue->enabled */
+	rx_queue->enabled = false;
 
 	del_timer_sync(&rx_queue->slow_fill);
 	efx_nic_fini_rx(rx_queue);
