558d1fbeaebba05a5529cbd2b28f43d6a67e861c
Benjamin Poirier <bpoirier@suse.de>
2013-02-01 16:19:30 -0500

    Refresh patches.drivers/rename-dev_hw_addr_random-and-remove-redundant-secon.patch.
    suse-commit: ff5854104c016d142338dda1c8537897b0dedb36

--- drivers/net/ixgbevf/ixgbevf_main.c
+++ drivers/net/ixgbevf/ixgbevf_main.c
@@ -2216,17 +2216,13 @@ static int __devinit ixgbevf_sw_init(struct ixgbevf_adapter *adapter)
 	if (err) {
 		dev_info(&pdev->dev,
 		         "PF still in reset state, assigning new address\n");
-		eth_hw_addr_random(adapter->netdev);
-		memcpy(adapter->hw.mac.addr, adapter->netdev->dev_addr,
-			adapter->netdev->addr_len);
+		dev_hw_addr_random(adapter->netdev, hw->mac.addr);
 	} else {
 		err = hw->mac.ops.init_hw(hw);
 		if (err) {
 			printk(KERN_ERR "init_shared_code failed: %d\n", err);
 			goto out;
 		}
-		memcpy(adapter->netdev->dev_addr, adapter->hw.mac.addr,
-			adapter->netdev->addr_len);
 	}
 
 	/* Enable dynamic interrupt throttling rates */
@@ -2245,7 +2241,6 @@ static int __devinit ixgbevf_sw_init(struct ixgbevf_adapter *adapter)
 	adapter->flags |= IXGBE_FLAG_RX_CSUM_ENABLED;
 
 	set_bit(__IXGBEVF_DOWN, &adapter->state);
-	return 0;
 
 out:
 	return err;
@@ -3377,8 +3372,6 @@ static int __devinit ixgbevf_probe(struct pci_dev *pdev,
 
 	/* setup the private structure */
 	err = ixgbevf_sw_init(adapter);
-	if (err)
-		goto err_sw_init;
 
 	netdev->features = NETIF_F_SG |
 			   NETIF_F_IP_CSUM |
