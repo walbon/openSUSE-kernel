4a7273711478abf1aaaee5c3aa1d9d3b2f3ffb3d
Bruce Rogers <brogers@suse.com>
2013-02-06 21:02:26 -0700

    x86, apicv: add APICv register virtualization support (fate#313618).
    suse-commit: 0fb9648c7132e578cb37e13190610fbecde587f1

--- arch/x86/kvm/lapic.c
+++ arch/x86/kvm/lapic.c
@@ -1158,6 +1158,21 @@ static int apic_mmio_write(struct kvm_io_device *this,
 	return 0;
 }
 
+/* emulate APIC access in a trap manner */
+void kvm_apic_write_nodecode(struct kvm_vcpu *vcpu, u32 offset)
+{
+	u32 val = 0;
+
+	/* hw has done the conditional check and inst decode */
+	offset &= 0xff0;
+
+	apic_reg_read(vcpu->arch.apic, offset, 4, &val);
+
+	/* TODO: optimize to just emulate side effect w/o one more write */
+	apic_reg_write(vcpu->arch.apic, offset, val);
+}
+EXPORT_SYMBOL_GPL(kvm_apic_write_nodecode);
+
 void kvm_free_lapic(struct kvm_vcpu *vcpu)
 {
 	if (!vcpu->arch.apic)
