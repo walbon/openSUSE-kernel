70be27bf31e454930aa3b6d00fbda230e3aede3e
Michal Hocko <mhocko@suse.cz>
2013-02-11 17:26:36 +0100

    kexec: add the values related to buddy system for filtering free pages (bnc#802204).
    suse-commit: cc849a89bc6ddf542dbce54b78d01900eb11a621

--- kernel/kexec.c
+++ kernel/kexec.c
@@ -1595,6 +1595,8 @@ static int __init crash_save_vmcoreinfo_init(void)
 	VMCOREINFO_OFFSET(page, _count);
 	VMCOREINFO_OFFSET(page, mapping);
 	VMCOREINFO_OFFSET(page, lru);
+	VMCOREINFO_OFFSET(page, _mapcount);
+	VMCOREINFO_OFFSET(page, private);
 	VMCOREINFO_OFFSET(pglist_data, node_zones);
 	VMCOREINFO_OFFSET(pglist_data, nr_zones);
 #ifdef CONFIG_FLAT_NODE_MEM_MAP
@@ -1622,6 +1624,8 @@ static int __init crash_save_vmcoreinfo_init(void)
 	VMCOREINFO_NUMBER(PG_compound);
 #endif
 	VMCOREINFO_NUMBER(PG_swapcache);
+	VMCOREINFO_NUMBER(PG_slab);
+	VMCOREINFO_NUMBER(PAGE_BUDDY_MAPCOUNT_VALUE);
 
 	arch_crash_save_vmcoreinfo();
 	update_vmcoreinfo_note();
