95f67ca1b65861fe2d20e434b386a090fe4454e8
Michal Srb <msrb@novell.com>
2013-02-12 17:09:12 +0100

    drm/radeon: Simplify cursor x/yorigin calculation.
    suse-commit: 4d27a7fdc38dce594a091af1ca5f314d098e5ba6

--- drivers/gpu/drm/radeon/radeon_cursor.c
+++ drivers/gpu/drm/radeon/radeon_cursor.c
@@ -225,13 +225,9 @@ int radeon_crtc_cursor_move(struct drm_crtc *crtc,
 	DRM_DEBUG("x %d y %d c->x %d c->y %d\n", x, y, crtc->x, crtc->y);
 
 	if (x < 0)
-		xorigin = -x;
+		xorigin = min(-x, CURSOR_WIDTH - 1);
 	if (y < 0)
-		yorigin = -y;
-	if (xorigin >= CURSOR_WIDTH)
-		xorigin = CURSOR_WIDTH - 1;
-	if (yorigin >= CURSOR_HEIGHT)
-		yorigin = CURSOR_HEIGHT - 1;
+		yorigin = min(-y, CURSOR_HEIGHT - 1);
 
 	if (ASIC_IS_AVIVO(rdev)) {
 		int i = 0;
