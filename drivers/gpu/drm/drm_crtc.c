0eb02c93e6a70bbfa60632a2df45112e08076138
Michal Srb <msrb@novell.com>
2013-02-13 14:02:14 +0100

    drm/usb: move usb support into a separate module.
    suse-commit: 264b04b2e9ebf2b9a83f3d8c9045255fe7bae125

--- drivers/gpu/drm/drm_crtc.c
+++ drivers/gpu/drm/drm_crtc.c
@@ -985,6 +985,7 @@ int drm_mode_group_init_legacy_group(struct drm_device *dev,
 
 	return 0;
 }
+EXPORT_SYMBOL(drm_mode_group_init_legacy_group);
 
 /**
  * drm_mode_config_cleanup - free up DRM mode_config info
