1374cc59d5106c2308a99c7beabc64278049c1e0
Michal Srb <msrb@novell.com>
2013-02-13 15:58:10 +0100

    drm/i915: fix EDID memory leak in SDVO.
    suse-commit: 1815bed29a24243f3282cad0a1311b2eda866848

--- drivers/gpu/drm/i915/intel_sdvo.c
+++ drivers/gpu/drm/i915/intel_sdvo.c
@@ -1713,6 +1713,7 @@ static bool intel_sdvo_detect_hdmi_audio(struct drm_connector *connector)
 	edid = intel_sdvo_get_edid(connector);
 	if (edid != NULL && edid->input & DRM_EDID_INPUT_DIGITAL)
 		has_audio = drm_detect_monitor_audio(edid);
+	kfree(edid);
 
 	return has_audio;
 }
