331ecf8fd86142a2f60ea4ee2f7c2c43139e840f
Michal Srb <msrb@novell.com>
2013-02-13 17:10:53 +0100

    cirrus: Kms only for secure boot.
    suse-commit: f9a73ce0d64ad697f7379d98213b92d772c540ea

--- drivers/gpu/drm/cirrus/cirrus_drv.c
+++ drivers/gpu/drm/cirrus/cirrus_drv.c
@@ -10,6 +10,7 @@
  */
 #include <linux/module.h>
 #include <linux/console.h>
+#include <linux/capability.h>
 #include <drm/drmP.h>
 
 #include "cirrus_drv.h"
@@ -117,6 +118,13 @@ static int __init cirrus_init(void)
 #ifdef CONFIG_VGA_CONSOLE
 	if (vgacon_text_force() && cirrus_modeset == -1)
 		return -EINVAL;
+#endif
+
+#ifdef CAP_COMPROMISE_KERNEL
+	if (capable(CAP_COMPROMISE_KERNEL) && cirrus_modeset == -1) {
+		printk(KERN_INFO "This driver is only used in secure boot mode as default\n");
+		return -EINVAL;
+	}
 #endif
 
 	if (cirrus_modeset == 0)
