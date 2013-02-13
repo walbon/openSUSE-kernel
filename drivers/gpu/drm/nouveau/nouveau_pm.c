c1c91cc0e6b0a9cce1f1ddd1d6a074b0f341ab86
Michal Srb <msrb@novell.com>
2013-02-13 16:35:28 +0100

    drm/nouveau/pm: do not stop reclocking if failing to set the fan speed.
    suse-commit: cb38e2ef6b279a88d74b53e7dad1458785981145

--- drivers/gpu/drm/nouveau/nouveau_pm.c
+++ drivers/gpu/drm/nouveau/nouveau_pm.c
@@ -129,7 +129,6 @@ nouveau_pm_perflvl_aux(struct drm_device *dev, struct nouveau_pm_level *perflvl,
 		ret = nouveau_pwmfan_set(dev, perflvl->fanspeed);
 		if (ret && ret != -ENODEV) {
 			NV_ERROR(drm, "fanspeed set failed: %d\n", ret);
-			return ret;
 		}
 	}
 
