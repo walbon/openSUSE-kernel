d7222ab933d47cd0de4807c60cec8849eb388ae4
Michal Srb <msrb@novell.com>
2013-02-13 17:02:01 +0100

    DRM/i915: Don't clone SDVO LVDS with analog.
    suse-commit: 05750995a2f03259ff09f6d8e575332985b7f59b

--- drivers/gpu/drm/i915/intel_sdvo.c
+++ drivers/gpu/drm/i915/intel_sdvo.c
@@ -2294,6 +2294,9 @@ intel_sdvo_lvds_init(struct intel_sdvo *intel_sdvo, int device)
 		intel_sdvo_connector->output_flag = SDVO_OUTPUT_LVDS1;
 	}
 
+	/* SDVO LVDS is not cloneable because the input mode gets adjusted by the encoder */
+	intel_sdvo->base.cloneable = false;
+
 	intel_sdvo_connector_init(intel_sdvo_connector, intel_sdvo);
 	if (!intel_sdvo_create_enhance_property(intel_sdvo, intel_sdvo_connector))
 		goto err;
