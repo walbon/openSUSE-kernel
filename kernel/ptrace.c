39d120dd2c7e54b93cdbe5d975624b0827b10ab4
Michal Hocko <mhocko@suse.cz>
2013-02-19 11:58:02 +0100

    - patches.fixes/ptrace-ensure-arch_ptrace-ptrace_request-can-never-r.patch:   Refresh.
    suse-commit: 81dbbc9e8633c350ebfc2e6a6f0e016728485d7c

--- kernel/ptrace.c
+++ kernel/ptrace.c
@@ -149,12 +149,12 @@ int ptrace_check_attach(struct task_struct *child, int kill)
 		 * child->sighand can't be NULL, release_task()
 		 * does ptrace_unlink() before __exit_signal().
 		 */
-		if (ignore_state || ptrace_freeze_traced(child))
+		if (kill || ptrace_freeze_traced(child))
 			ret = 0;
 	}
 	read_unlock(&tasklist_lock);
 
-	if (!ret && !ignore_state) {
+	if (!ret && !kill) {
 		if (!wait_task_inactive(child, __TASK_TRACED)) {
 			/*
 			 * This can only happen if may_ptrace_stop() fails and
