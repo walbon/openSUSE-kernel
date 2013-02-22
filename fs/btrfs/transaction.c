3097d5fcec8b12d5992f6d7d855e309e849b0868
David Sterba <dsterba@suse.cz>
2013-02-22 01:23:07 +0100

    - Btrfs: Fix typo in fs/btrfs. - Btrfs: don't start a new transaction when starting sync   (FATE#312888). - Refresh   patches.suse/btrfs-0852-recheck-bio-against-block-device-when-we-map-t.patch. - Refresh   patches.suse/btrfs-8189-use-common-work-instead-of-delayed-work.patch. - Delete patches.suse/btrfs-8202-Fix-typo-in-fs-btrfs.patch.
    suse-commit: 16a9d7d11afb67340a3a3897b82cbe87286c7e14

--- fs/btrfs/transaction.c
+++ fs/btrfs/transaction.c
@@ -1316,6 +1316,13 @@ static void do_async_commit(struct work_struct *work)
 	struct btrfs_async_commit *ac =
 		container_of(work, struct btrfs_async_commit, work);
 
+	/*
+	if (ac->newtrans->type < TRANS_JOIN_NOLOCK)
+		rwsem_acquire_read(
+		     &ac->root->fs_info->sb->s_writers.lock_map[SB_FREEZE_FS-1],
+		     0, 1, _THIS_IP_);
+	*/
+
 	current->journal_info = ac->newtrans;
 
 	btrfs_commit_transaction(ac->newtrans, ac->root);
@@ -1347,6 +1354,12 @@ int btrfs_commit_transaction_async(struct btrfs_trans_handle *trans,
 	atomic_inc(&cur_trans->use_count);
 
 	btrfs_end_transaction(trans, root);
+	/*
+	if (trans->type < TRANS_JOIN_NOLOCK)
+		rwsem_release(
+			&root->fs_info->sb->s_writers.lock_map[SB_FREEZE_FS-1],
+			1, _THIS_IP_);
+	*/
 	schedule_work(&ac->work);
 
 	/* wait for transaction to start and unblock */
