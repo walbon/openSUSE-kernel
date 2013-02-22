28203dabf093c4c2add6e684dd2bc808f6268cb7
Benjamin Poirier <bpoirier@suse.de>
2013-02-21 21:36:44 -0500

    net/mlx4_core: Fixes for VF / Guest startup flow (bnc#786036 FATE#314304).
    suse-commit: 270d12b037970eef67bdbd2e95a22db955ce3c4e

--- drivers/net/ethernet/mellanox/mlx4/cmd.c
+++ drivers/net/ethernet/mellanox/mlx4/cmd.c
@@ -616,7 +616,7 @@ static struct mlx4_cmd_info cmd_info[] = {
 		.out_is_imm = false,
 		.encode_slave_id = false,
 		.verify = NULL,
-		.wrapper = NULL
+		.wrapper = mlx4_QUERY_FW_wrapper
 	},
 	{
 		.opcode = MLX4_CMD_QUERY_HCA,
@@ -634,7 +634,7 @@ static struct mlx4_cmd_info cmd_info[] = {
 		.out_is_imm = false,
 		.encode_slave_id = false,
 		.verify = NULL,
-		.wrapper = NULL
+		.wrapper = mlx4_QUERY_DEV_CAP_wrapper
 	},
 	{
 		.opcode = MLX4_CMD_QUERY_FUNC_CAP,
