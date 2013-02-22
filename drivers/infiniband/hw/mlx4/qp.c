8ef228cf65674b091222728fbc70712973f6d507
Benjamin Poirier <bpoirier@suse.de>
2013-02-21 21:31:06 -0500

    IB/mlx4: Set bad_wr for invalid send opcode (bnc#786036 FATE#314304).
    suse-commit: ae22da30b5105b3f38bd8c2d294978c25a687875

--- drivers/infiniband/hw/mlx4/qp.c
+++ drivers/infiniband/hw/mlx4/qp.c
@@ -1876,6 +1876,7 @@ int mlx4_ib_post_send(struct ib_qp *ibqp, struct ib_send_wr *wr,
 		wmb();
 
 		if (wr->opcode < 0 || wr->opcode >= ARRAY_SIZE(mlx4_ib_opcode)) {
+			*bad_wr = wr;
 			err = -EINVAL;
 			goto out;
 		}
