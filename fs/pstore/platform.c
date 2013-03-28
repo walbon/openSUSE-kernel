3584aa035b38d38a4be749001f5c0bc6f4d8793c
Thomas Renninger <trenn@suse.de>
2013-03-28 10:33:48 +0100

    Refresh patches.drivers/pstore_disable_efi_backend_by_default.patch. Remove harsh warning message -> Samsung bricking issue should have been fixed.
    suse-commit: a4eeafccbd625075316f589806fb2c63814fed92

--- fs/pstore/platform.c
+++ fs/pstore/platform.c
@@ -186,8 +186,6 @@ int pstore_register(struct pstore_info *psi)
 
 	if (!backend && !strcmp(psi->name, "efi")) {
 		pr_info("Efi pstore disabled, enforce via pstore.backend=efi");
-		pr_info("On a broken BIOS, this can severely harm your system");
-		pr_info("Only enable efi based pstore when you know what you are doing");
 		spin_unlock(&pstore_lock);
 		return -EINVAL;
 	}
