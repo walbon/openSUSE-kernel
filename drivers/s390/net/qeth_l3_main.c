1d9029242f97198045e10b0207c7bde953dc806a
John Jolly <jjolly@suse.de>
2013-03-29 15:20:29 +0100

    - patches.arch/s390-65-01-qeth-fix_wait_for_threads_deadlock.patch:   qeth: fix qeth_wait_for_threads() deadlock for OSN devices   (bnc#812315, LTC#90910).
    suse-commit: d3540df6e8519d641583c5fc82b39195b6429ea1

--- drivers/s390/net/qeth_l3_main.c
+++ drivers/s390/net/qeth_l3_main.c
@@ -3575,6 +3575,7 @@ static int qeth_l3_recover(void *ptr)
 	QETH_CARD_TEXT(card, 2, "recover2");
 	dev_warn(&card->gdev->dev,
 		"A recovery process has been started for the device\n");
+	qeth_set_recovery_task(card);
 	__qeth_l3_set_offline(card->gdev, 1);
 	rc = __qeth_l3_set_online(card->gdev, 1);
 	if (!rc)
@@ -3588,6 +3589,7 @@ static int qeth_l3_recover(void *ptr)
 				"failed to recover an error on the device\n");
 		}
 	}
+	qeth_clear_recovery_task(card);
 	qeth_clear_thread_start_bit(card, QETH_RECOVER_THREAD);
 	qeth_clear_thread_running_bit(card, QETH_RECOVER_THREAD);
 	return 0;
