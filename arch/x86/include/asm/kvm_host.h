d34afd89b06ba73933ff0a35955fafa2955e3a67
Bruce Rogers <brogers@suse.com>
2013-05-09 16:52:25 -0600

    KVM: x86: Increase the "hard" max VCPU limit (bnc#819243).
    suse-commit: 79472f5fd741216d468ecc4271793ce28325878e

--- arch/x86/include/asm/kvm_host.h
+++ arch/x86/include/asm/kvm_host.h
@@ -26,7 +26,7 @@
 #include <asm/mtrr.h>
 #include <asm/msr-index.h>
 
-#define KVM_MAX_VCPUS 254
+#define KVM_MAX_VCPUS 255
 #define KVM_SOFT_MAX_VCPUS 160
 #define KVM_MEMORY_SLOTS 32
 /* memory slots that does not exposed to userspace */
