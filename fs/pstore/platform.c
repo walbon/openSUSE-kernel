321bbff07525891d6edca55ab72f10a37a6c4112
Thomas Renninger <trenn@suse.de>
2013-06-06 16:10:33 +0200

    - patches.drivers/pstore_disable_efi_backend_by_default.patch:   Disable efi pstore by default (bnc#804482 bnc#820172).
    suse-commit: bb15ef604ec10893c810f391dbae69d5ee4478b0

--- fs/pstore/platform.c
+++ fs/pstore/platform.c
@@ -161,6 +161,12 @@ int pstore_register(struct pstore_info *psi)
 		return -EBUSY;
 	}
 
+	if (!backend && !strcmp(psi->name, "efi")) {
+		pr_info("Efi pstore disabled, enforce via pstore.backend=efi");
+		spin_unlock(&pstore_lock);
+		return -EINVAL;
+	}
+
 	if (backend && strcmp(backend, psi->name)) {
 		spin_unlock(&pstore_lock);
 		return -EINVAL;
