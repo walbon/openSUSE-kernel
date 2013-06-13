0782a86028eef02a1c94278fe7b6bad490f85f51
Benjamin Poirier <bpoirier@suse.de>
2013-06-13 11:04:13 -0400

    netiucv: Hold rtnl between name allocation and device registration (bnc#824159).
    suse-commit: c051cd30a037c854fcfa81e6a11e5018a7309e4b

--- drivers/s390/net/netiucv.c
+++ drivers/s390/net/netiucv.c
@@ -1994,6 +1994,7 @@ static struct net_device *netiucv_init_netdevice(char *username)
 			   netiucv_setup_netdevice);
 	if (!dev)
 		return NULL;
+	rtnl_lock();
 	if (dev_alloc_name(dev, dev->name) < 0)
 		goto out_netdev;
 
@@ -2015,6 +2016,7 @@ static struct net_device *netiucv_init_netdevice(char *username)
 out_fsm:
 	kfree_fsm(privptr->fsm);
 out_netdev:
+	rtnl_unlock();
 	free_netdev(dev);
 	return NULL;
 }
@@ -2070,6 +2072,7 @@ static ssize_t conn_write(struct device_driver *drv,
 
 	rc = netiucv_register_device(dev);
 	if (rc) {
+		rtnl_unlock();
 		IUCV_DBF_TEXT_(setup, 2,
 			"ret %d from netiucv_register_device\n", rc);
 		goto out_free_ndev;
@@ -2079,7 +2082,8 @@ static ssize_t conn_write(struct device_driver *drv,
 	priv = netdev_priv(dev);
 	SET_NETDEV_DEV(dev, priv->dev);
 
-	rc = register_netdev(dev);
+	rc = register_netdevice(dev);
+	rtnl_unlock();
 	if (rc)
 		goto out_unreg;
 
