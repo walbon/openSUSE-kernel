bff7efc77c007f3434a9c8274691713dcede88c2
Oliver Neukum <oneukum@suse.de>
2013-07-15 14:51:35 +0200

    ACPI:remove panic in case hardware has changed after S4 (bnc#829001).
    suse-commit: 3cf00f34abd68ec3f29aa015586cc4e79cc00fd9

--- drivers/acpi/sleep.c
+++ drivers/acpi/sleep.c
@@ -506,9 +506,8 @@ static void acpi_hibernation_leave(void)
 	acpi_leave_sleep_state_prep(ACPI_STATE_S4);
 	/* Check the hardware signature */
 	if (facs && s4_hardware_signature != facs->hardware_signature) {
-		printk(KERN_EMERG "ACPI: Hardware changed while hibernated, "
-			"cannot resume!\n");
-		panic("ACPI S4 hardware signature mismatch");
+		printk(KERN_CRIT "ACPI: Hardware changed while hibernated, "
+			"success doubtful!\n");
 	}
 	/* Restore the NVS memory area */
 	suspend_nvs_restore();
