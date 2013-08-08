abefb0f5e746bbd80be874a3e1eecf16642a1e2a
Danny Al-Gaaf <dalgaaf@suse.de>
2013-08-08 14:58:57 +0200

    libceph: set page alignment in start_request() (fate#312983).
    suse-commit: 81740bbf4e8756b37dd46b0e7572c21d26ca00d7

--- net/ceph/osd_client.c
+++ net/ceph/osd_client.c
@@ -399,7 +399,6 @@ void ceph_osdc_build_request(struct ceph_osd_request *req,
 		data_len += len;
 	}
 	req->r_request->hdr.data_len = cpu_to_le32(data_len);
-	req->r_request->page_alignment = req->r_page_alignment;
 
 	BUG_ON(p > msg->front.iov_base + msg->front.iov_len);
 	msg_size = p - msg->front.iov_base;
@@ -1742,7 +1741,8 @@ int ceph_osdc_start_request(struct ceph_osd_client *osdc,
 	int rc = 0;
 
 	req->r_request->pages = req->r_pages;
-	req->r_request->nr_pages = req->r_num_pages;
+	req->r_request->page_count = req->r_num_pages;
+	req->r_request->page_alignment = req->r_page_alignment;
 #ifdef CONFIG_BLOCK
 	req->r_request->bio = req->r_bio;
 #endif
@@ -2093,7 +2093,7 @@ static struct ceph_msg *get_reply(struct ceph_connection *con,
 			goto out;
 		}
 		m->pages = req->r_pages;
-		m->nr_pages = req->r_num_pages;
+		m->page_count = req->r_num_pages;
 		m->page_alignment = req->r_page_alignment;
 #ifdef CONFIG_BLOCK
 		m->bio = req->r_bio;
