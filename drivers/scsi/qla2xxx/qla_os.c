c9b5bd7b511590b7758f1e8d2b54ac38888dbda8
Hannes Reinecke <hare@suse.de>
2013-11-04 11:51:19 +0100

    - patches.drivers/qla2xxx-Module-parameter-ql2xasynclogin.patch:   qla2xxx: Module parameter 'ql2xasynclogin' (bnc#825896).
    suse-commit: 6f4951af49d589eb9757eca854049fce279f76e6

--- drivers/scsi/qla2xxx/qla_os.c
+++ drivers/scsi/qla2xxx/qla_os.c
@@ -207,6 +207,13 @@ MODULE_PARM_DESC(ql2xmdenable,
 		"0 (Default) - MiniDump disabled. "
 		"1 - MiniDump enabled.");
 
+int ql2xasynclogin = 1;
+module_param(ql2xasynclogin, int, S_IRUGO);
+MODULE_PARM_DESC(ql2xasynclogin,
+		"Enables asynchronous login to remote ports "
+		"Default is 1 - Use asynchronous login if possible.");
+
+
 /*
  * SCSI host template entry points
  */
@@ -3527,7 +3534,7 @@ void qla2x00_relogin(struct scsi_qla_host *vha)
 					}
 				}
 
-				if (IS_ALOGIO_CAPABLE(ha)) {
+				if (ql2xasynclogin && IS_ALOGIO_CAPABLE(ha)) {
 					fcport->flags |= FCF_ASYNC_SENT;
 					data[0] = 0;
 					data[1] = QLA_LOGIO_LOGIN_RETRIED;
