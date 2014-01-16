3119003cbc0c7b447de8f902bdbb00b968c0c86d
Hannes Reinecke <hare@suse.de>
2014-01-16 08:50:17 +0100

    Delete patches.fixes/dm-mpath-abort-all-requests-when-failing-a-path.patch.
    suse-commit: b9b9940d87abf07731b4fa65f4096fa87cd909a7

--- drivers/md/dm-mpath.c
+++ drivers/md/dm-mpath.c
@@ -1059,9 +1059,6 @@ static int fail_path(struct pgpath *pgpath)
 	if (pgpath == m->current_pgpath)
 		m->current_pgpath = NULL;
 
-	if (pgpath->path.dev)
-		blk_abort_queue(bdev_get_queue(pgpath->path.dev->bdev));
-
 	dm_path_uevent(DM_UEVENT_PATH_FAILED, m->ti,
 		       pgpath->path.pdev, m->nr_valid_paths);
 
