f3cfb15cd4fdea64c491c55f369d9480aad6422d
Michal Marek <mmarek@suse.cz>
2014-05-05 13:21:43 +0200

    Update patches.drivers/0001-ACPI-remove-panic-in-case-hardware-has-changed-after.patch from upstream
    suse-commit: e4478d5d3dd056859a131ec83389bd41820e6738

--- drivers/acpi/sleep.c
+++ drivers/acpi/sleep.c
@@ -505,10 +505,8 @@ static void acpi_hibernation_leave(void)
 	/* Reprogram control registers and execute _BFS */
 	acpi_leave_sleep_state_prep(ACPI_STATE_S4);
 	/* Check the hardware signature */
-	if (facs && s4_hardware_signature != facs->hardware_signature) {
-		printk(KERN_CRIT "ACPI: Hardware changed while hibernated, "
-			"success doubtful!\n");
-	}
+	if (facs && s4_hardware_signature != facs->hardware_signature)
+		pr_crit("ACPI: Hardware changed while hibernated, success doubtful!\n");
 	/* Restore the NVS memory area */
 	suspend_nvs_restore();
 	/* Allow EC transactions to happen. */
