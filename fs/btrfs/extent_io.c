f030c7f1fda0c8590bd23b640cf20faa22c53b2e
David Sterba <dsterba@suse.cz>
2014-06-20 11:04:47 +0200

    patches.suse/btrfs-8234-more-efficient-io-tree-navigation-on-wait_exte.patch:
    suse-commit: 99100fb980ce5be1b6448cb2c88a26943252d2f1

--- fs/btrfs/extent_io.c
+++ fs/btrfs/extent_io.c
@@ -658,6 +658,7 @@ again:
 		 * our range starts
 		 */
 		node = tree_search(tree, start);
+process_node:
 		if (!node)
 			break;
 
@@ -678,7 +679,10 @@ again:
 		if (start > end)
 			break;
 
-		cond_resched_lock(&tree->lock);
+		if (!cond_resched_lock(&tree->lock)) {
+			node = rb_next(node);
+			goto process_node;
+		}
 	}
 out:
 	spin_unlock(&tree->lock);
