401c2c863e3c14065401c4480e59b1ee3def57d3
Vlastimil Babka <vbabka@suse.cz>
2014-07-24 11:52:15 +0200

    - shmem: fix faulting into a hole, not taking i_mutex (bnc#883518,   CVE-2014-4171). - shmem: fix splicing from a hole while it's punched (bnc#883518,   CVE-2014-4171). - Delete   patches.fixes/shmem-fix-faulting-into-a-hole-while-its-punched-take-2.patch.
    suse-commit: 634b83f2de79dd1063e0d46e680d01bbd78b3d2a

--- mm/truncate.c
+++ mm/truncate.c
@@ -603,28 +603,3 @@ int vmtruncate(struct inode *inode, loff_t newsize)
 }
 EXPORT_SYMBOL(vmtruncate);
 
-int vmtruncate_range(struct inode *inode, loff_t lstart, loff_t lend)
-{
-	struct address_space *mapping = inode->i_mapping;
-	loff_t holebegin = round_up(lstart, PAGE_SIZE);
-	loff_t holelen = 1 + lend - holebegin;
-
-	/*
-	 * If the underlying filesystem is not going to provide
-	 * a way to truncate a range of blocks (punch a hole) -
-	 * we should return failure right now.
-	 */
-	if (!inode->i_op->truncate_range)
-		return -ENOSYS;
-
-	mutex_lock(&inode->i_mutex);
-	down_write(&inode->i_alloc_sem);
-	unmap_mapping_range(mapping, holebegin, holelen, 1);
-	inode->i_op->truncate_range(inode, lstart, lend);
-	/* unmap again to remove racily COWed private pages */
-	unmap_mapping_range(mapping, holebegin, holelen, 1);
-	up_write(&inode->i_alloc_sem);
-	mutex_unlock(&inode->i_mutex);
-
-	return 0;
-}
