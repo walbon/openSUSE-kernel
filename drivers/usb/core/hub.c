ed85f4e0af240bbf3a71aa5e57eeda3ca7b33d3d
Oliver Neukum <oneukum@suse.de>
2014-07-25 17:01:26 +0200

    usbcore: don't log on consecutive debounce failures of the same port (bnc#888105).
    suse-commit: 867a600cb5a78f17b1dfd1291d9ad33d6758e7e0

--- drivers/usb/core/hub.c
+++ drivers/usb/core/hub.c
@@ -4143,6 +4143,7 @@ static void hub_port_connect_change(struct usb_hub *hub, int port1,
 			le16_to_cpu(hub->descriptor->wHubCharacteristics);
 	struct usb_device *udev;
 	int status, i;
+	static int unreliable_port = -1;
 
 	dev_dbg (hub_dev,
 		"port %d, status %04x, change %04x, %s\n",
@@ -4204,10 +4205,13 @@ static void hub_port_connect_change(struct usb_hub *hub, int port1,
 				USB_PORT_STAT_C_ENABLE)) {
 		status = hub_port_debounce(hub, port1);
 		if (status < 0) {
-			if (printk_ratelimit())
-				dev_err(hub_dev, "connect-debounce failed, "
-						"port %d disabled\n", port1);
+			if (status != -ENODEV &&
+				port1 != unreliable_port &&
+				printk_ratelimit())
+				dev_err(hub_dev, "connect-debounce failed, port %d disabled\n",
+					 port1);
 			portstatus &= ~USB_PORT_STAT_CONNECTION;
+			unreliable_port = port1;
 		} else {
 			portstatus = status;
 		}
