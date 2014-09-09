55be4c68d4345ff9f4b4f233cefc78b19f33e26e
Petr Tesarik <ptesarik@suse.cz>
2014-09-09 10:22:57 +0200

    memcg: do not expose uninitialized mem_cgroup_per_node to world (bnc#883096).
    suse-commit: ae8260779d927cb21e6cd32db0a4018789ea45bf

--- mm/memcontrol.c
+++ mm/memcontrol.c
@@ -4847,7 +4847,6 @@ static int alloc_mem_cgroup_per_zone_info(struct mem_cgroup *mem, int node)
 	if (!pn)
 		return 1;
 
-	mem->info.nodeinfo[node] = pn;
 	for (zone = 0; zone < MAX_NR_ZONES; zone++) {
 		mz = &pn->zoneinfo[zone];
 		for_each_lru(l)
@@ -4856,6 +4855,7 @@ static int alloc_mem_cgroup_per_zone_info(struct mem_cgroup *mem, int node)
 		mz->on_tree = false;
 		mz->mem = mem;
 	}
+	mem->info.nodeinfo[node] = pn;
 	return 0;
 }
 
