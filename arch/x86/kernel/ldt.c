3b42e4d6e2d5ba5fd4e703941d4edf32917f80d2
Borislav Petkov <bp@suse.de>
2014-12-16 14:28:08 +0100

    x86, espfix: Make it possible to disable 16-bit support (bsc#907818,CVE-2014-9090).
    suse-commit: 56a3f48222ea406d1198b9aeefdb622ac586025b

--- arch/x86/kernel/ldt.c
+++ arch/x86/kernel/ldt.c
@@ -230,6 +230,13 @@ static int write_ldt(void __user *ptr, unsigned long bytecount, int oldmode)
 		}
 	}
 
+#ifndef CONFIG_X86_16BIT
+	if (!ldt_info.seg_32bit) {
+		error = -EINVAL;
+		goto out_unlock;
+	}
+#endif
+
 	fill_ldt(&ldt, &ldt_info);
 	if (oldmode)
 		ldt.avl = 0;
