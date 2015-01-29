f93f7c7fbc1ade65739e8f5e3601ae774c027d72
Cho, Yu-Chen <acho@suse.com>
2015-01-29 12:36:20 +0800

    bnx2[x]: Make module parameters readable (bsc#908686 FATE#317538).
    suse-commit: 16a1e9c72681e30127024c17ebb50b0ef0841eb2

--- drivers/net/ethernet/broadcom/bnx2.c
+++ drivers/net/ethernet/broadcom/bnx2.c
@@ -88,7 +88,7 @@ MODULE_PARM_DESC(entropy, "Allow bnx2 to populate the /dev/random entropy pool")
 
 static int disable_msi = 0;
 
-module_param(disable_msi, int, 0);
+module_param(disable_msi, int, S_IRUGO);
 MODULE_PARM_DESC(disable_msi, "Disable Message Signaled Interrupt (MSI)");
 
 typedef enum {
