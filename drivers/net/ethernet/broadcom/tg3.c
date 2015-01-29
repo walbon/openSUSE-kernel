75338238c1762ec915bbec260a0091ecdc5808c9
Cho, Yu-Chen <acho@suse.com>
2015-01-29 16:28:27 +0800

    net: tg3: remove unnecessary pci_set_drvdata() (bsc#908458 FATE#317507).
    suse-commit: 31d46677c11f61fa55fc70e77482b3ba198bbe67

--- drivers/net/ethernet/broadcom/tg3.c
+++ drivers/net/ethernet/broadcom/tg3.c
@@ -17631,7 +17631,6 @@ err_out_free_res:
 err_out_disable_pdev:
 	if (pci_is_enabled(pdev))
 		pci_disable_device(pdev);
-	pci_set_drvdata(pdev, NULL);
 	return err;
 }
 
@@ -17663,7 +17662,6 @@ static void tg3_remove_one(struct pci_dev *pdev)
 		free_netdev(dev);
 		pci_release_regions(pdev);
 		pci_disable_device(pdev);
-		pci_set_drvdata(pdev, NULL);
 	}
 }
 
