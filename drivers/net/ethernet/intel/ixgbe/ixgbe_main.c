25e7cfaebdd3669234d71026e9856d3d830b4b4f
Benjamin Poirier <bpoirier@suse.de>
2015-02-12 13:21:28 +0800

    ixgbe: update date to 2013 (bug#908398 FATE#317382).
    suse-commit: 6f85f6a40f4b8f52a00eb335deb0d9ab7c224dbb

--- drivers/net/ethernet/intel/ixgbe/ixgbe_main.c
+++ drivers/net/ethernet/intel/ixgbe/ixgbe_main.c
@@ -1,7 +1,7 @@
 /*******************************************************************************
 
   Intel 10 Gigabit PCI Express Linux driver
-  Copyright(c) 1999 - 2012 Intel Corporation.
+  Copyright(c) 1999 - 2013 Intel Corporation.
 
   This program is free software; you can redistribute it and/or modify it
   under the terms and conditions of the GNU General Public License,
@@ -65,7 +65,7 @@ static char ixgbe_default_device_descr[] =
 #define DRV_VERSION "3.11.33-k"
 const char ixgbe_driver_version[] = DRV_VERSION;
 static const char ixgbe_copyright[] =
-				"Copyright (c) 1999-2012 Intel Corporation.";
+				"Copyright (c) 1999-2013 Intel Corporation.";
 
 static int entropy = 0;
 module_param(entropy, int, 0);
