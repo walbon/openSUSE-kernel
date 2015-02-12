3cb44f6c93d45242a27378f0c86602c0ab7d1c91
Benjamin Poirier <bpoirier@suse.de>
2015-02-12 13:21:28 +0800

    ixgbe: Add support for WoL on 82599 SFP+ LOM (bug#908398 FATE#317382).
    suse-commit: bcf15a58777b73246666100ff1e410ae553b876b

--- drivers/net/ethernet/intel/ixgbe/ixgbe_main.c
+++ drivers/net/ethernet/intel/ixgbe/ixgbe_main.c
@@ -7194,6 +7194,7 @@ int ixgbe_wol_supported(struct ixgbe_adapter *adapter, u16 device_id,
 		case IXGBE_SUBDEV_ID_82599_SFP:
 		case IXGBE_SUBDEV_ID_82599_RNDC:
 		case IXGBE_SUBDEV_ID_82599_ECNA_DP:
+		case IXGBE_SUBDEV_ID_82599_LOM_SFP:
 			is_wol_supported = 1;
 			break;
 		}
