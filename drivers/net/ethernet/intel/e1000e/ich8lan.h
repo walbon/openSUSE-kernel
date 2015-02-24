6008c12b44af593472fe8d0a5de2f333c2972dc2
Benjamin Poirier <bpoirier@suse.de>
2015-02-24 17:28:59 +0800

    e1000e: Fix 82579 sets LPI too early (bug#909495 FATE#317394).
    suse-commit: 50ccad873f59447ff2ed1d6ba1ac3f0eab2f5efa

--- drivers/net/ethernet/intel/e1000e/ich8lan.h
+++ drivers/net/ethernet/intel/e1000e/ich8lan.h
@@ -267,4 +267,5 @@ s32 e1000_configure_k1_ich8lan(struct e1000_hw *hw, bool k1_enable);
 void e1000_copy_rx_addrs_to_phy_ich8lan(struct e1000_hw *hw);
 s32 e1000_lv_jumbo_workaround_ich8lan(struct e1000_hw *hw, bool enable);
 s32 e1000_write_emi_reg_locked(struct e1000_hw *hw, u16 addr, u16 data);
+s32 e1000_set_eee_pchlan(struct e1000_hw *hw);
 #endif /* _E1000E_ICH8LAN_H_ */
