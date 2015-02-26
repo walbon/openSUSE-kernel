ccefc38ebaabe740083e05d8ad51910a9a1773e9
David Chang <dchang@suse.com>
2015-02-26 20:45:09 +0800

    sfc: Store port number in private data, not net_device::dev_id (bsc#909618 FATE#317521).
    suse-commit: 122e15937f65cc2f62573b9c5c9ee71ac38b8c42

--- drivers/net/ethernet/sfc/net_driver.h
+++ drivers/net/ethernet/sfc/net_driver.h
@@ -779,6 +779,7 @@ struct efx_nic {
 
 	char name[IFNAMSIZ];
 	struct pci_dev *pci_dev;
+	unsigned int port_num;
 	const struct efx_nic_type *type;
 	int legacy_irq;
 	bool legacy_irq_enabled;
@@ -911,7 +912,7 @@ static inline int efx_dev_registered(struct efx_nic *efx)
 
 static inline unsigned int efx_port_num(struct efx_nic *efx)
 {
-	return efx->net_dev->dev_id;
+	return efx->port_num;
 }
 
 /**
