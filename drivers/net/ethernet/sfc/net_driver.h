ab1b6249024a68a3481cd0102cb5b2ada975573f
David Chang <dchang@suse.com>
2015-02-26 20:45:09 +0800

    sfc: Add support for MCDI v2 (bsc#909618 FATE#317521).
    suse-commit: d80f4d3c5d862a3331cc09831db75058b0a921a1

--- drivers/net/ethernet/sfc/net_driver.h
+++ drivers/net/ethernet/sfc/net_driver.h
@@ -1024,6 +1024,7 @@ static inline unsigned int efx_port_num(struct efx_nic *efx)
  * @timer_period_max: Maximum period of interrupt timer (in ticks)
  * @offload_features: net_device feature flags for protocol offload
  *	features implemented in hardware
+ * @mcdi_max_ver: Maximum MCDI version supported
  */
 struct efx_nic_type {
 	int (*probe)(struct efx_nic *efx);
@@ -1100,6 +1101,7 @@ struct efx_nic_type {
 	unsigned int phys_addr_channels;
 	unsigned int timer_period_max;
 	u32 offload_features;
+	int mcdi_max_ver;
 };
 
 /**************************************************************************
