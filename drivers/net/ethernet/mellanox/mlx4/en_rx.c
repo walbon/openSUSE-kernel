1c1f73d38d842ab18da14d5d5be6ac5625880fc4
Benjamin Poirier <bpoirier@suse.de>
2015-03-12 16:58:56 -0700

    net/mlx4_core: Enforce irq affinity changes immediatly (bug#919382 FATE#317529).
    suse-commit: ffe4abe63614943840def356e4d1e974362bc110

--- drivers/net/ethernet/mellanox/mlx4/en_rx.c
+++ drivers/net/ethernet/mellanox/mlx4/en_rx.c
@@ -874,10 +874,17 @@ int mlx4_en_poll_rx_cq(struct napi_struct *napi, int budget)
 	done = mlx4_en_process_rx_cq(dev, cq, budget);
 
 	/* If we used up all the quota - we're probably not done yet... */
-	if (done == budget)
+	if (done == budget) {
 		INC_PERF_COUNTER(priv->pstats.napi_quota);
-	else {
+		if (unlikely(cq->mcq.irq_affinity_change)) {
+			cq->mcq.irq_affinity_change = false;
+			napi_complete(napi);
+			mlx4_en_arm_cq(priv, cq);
+			return 0;
+		}
+	} else {
 		/* Done for now */
+		cq->mcq.irq_affinity_change = false;
 		napi_complete(napi);
 		mlx4_en_arm_cq(priv, cq);
 	}
