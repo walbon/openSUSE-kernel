21f9a5c094f543a6078cb7ebc050bed563bb289e
Benjamin Poirier <bpoirier@suse.de>
2015-03-12 16:58:56 -0700

    net/mlx4_en: IRQ affinity hint is not cleared on port down (bug#919382 FATE#317529).
    suse-commit: e0a936f82432063ce1e1d3f21a801f9b0e53b1d8

--- drivers/net/ethernet/mellanox/mlx4/en_cq.c
+++ drivers/net/ethernet/mellanox/mlx4/en_cq.c
@@ -190,8 +190,6 @@ void mlx4_en_destroy_cq(struct mlx4_en_priv *priv, struct mlx4_en_cq **pcq)
 	mlx4_en_unmap_buffer(&cq->wqres.buf);
 	mlx4_free_hwq_res(mdev->dev, &cq->wqres, cq->buf_size);
 	if (priv->mdev->dev->caps.comp_pool && cq->vector) {
-		if (!cq->is_tx)
-			irq_set_affinity_hint(cq->mcq.irq, NULL);
 		mlx4_release_eq(priv->mdev->dev, cq->vector);
 	}
 	cq->vector = 0;
@@ -204,6 +202,10 @@ void mlx4_en_destroy_cq(struct mlx4_en_priv *priv, struct mlx4_en_cq **pcq)
 void mlx4_en_deactivate_cq(struct mlx4_en_priv *priv, struct mlx4_en_cq *cq)
 {
 	napi_disable(&cq->napi);
+	if (!cq->is_tx) {
+		synchronize_rcu();
+		irq_set_affinity_hint(cq->mcq.irq, NULL);
+	}
 	netif_napi_del(&cq->napi);
 
 	mlx4_cq_free(priv->mdev->dev, &cq->mcq);
