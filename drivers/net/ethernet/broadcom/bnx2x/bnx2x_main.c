566636fbaa47d06765595ccfca24963b757f057a
Cho, Yu-Chen <acho@suse.com>
2015-03-13 11:27:45 +0800

    net: bnx2x: remove unnecessary pci_set_drvdata() (bsc#908684 FATE#317539).
    suse-commit: 7c585f4fe42110d31ae0f3429eac6eee4174c0c2

--- drivers/net/ethernet/broadcom/bnx2x/bnx2x_main.c
+++ drivers/net/ethernet/broadcom/bnx2x/bnx2x_main.c
@@ -12274,7 +12274,6 @@ err_out_release:
 
 err_out_disable:
 	pci_disable_device(pdev);
-	pci_set_drvdata(pdev, NULL);
 
 err_out:
 	return rc;
@@ -12838,7 +12837,6 @@ init_one_exit:
 		pci_release_regions(pdev);
 
 	pci_disable_device(pdev);
-	pci_set_drvdata(pdev, NULL);
 
 	return rc;
 }
@@ -12921,7 +12919,6 @@ static void __bnx2x_remove(struct pci_dev *pdev,
 		pci_release_regions(pdev);
 
 	pci_disable_device(pdev);
-	pci_set_drvdata(pdev, NULL);
 }
 
 static void bnx2x_remove_one(struct pci_dev *pdev)
