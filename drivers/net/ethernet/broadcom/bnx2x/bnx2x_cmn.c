9097695a6ec3784478ce1a29c5fc2d90b4a44275
Cho, Yu-Chen <acho@suse.com>
2015-03-13 11:27:45 +0800

    bnx2x: Fix stop-on-error (bsc#908684 FATE#317539).
    suse-commit: c5f06c2b7a2e2fd5877a7d4c54588f508a528254

--- drivers/net/ethernet/broadcom/bnx2x/bnx2x_cmn.c
+++ drivers/net/ethernet/broadcom/bnx2x/bnx2x_cmn.c
@@ -3629,15 +3629,14 @@ netdev_tx_t bnx2x_start_xmit(struct sk_buff *skb, struct net_device *dev)
 		 * for fw to enforce it
 		 */
 #ifndef BNX2X_STOP_ON_ERROR
-		if (IS_VF(bp)) {
+		if (IS_VF(bp))
 #endif
 			tx_start_bd->vlan_or_ethertype =
 				cpu_to_le16(ntohs(eth->h_proto));
 #ifndef BNX2X_STOP_ON_ERROR
-		} else {
+		else
 			/* used by FW for packet accounting */
 			tx_start_bd->vlan_or_ethertype = cpu_to_le16(pkt_prod);
-		}
 #endif
 	}
 
