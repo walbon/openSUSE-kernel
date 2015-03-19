0c25ba1fb8f710e93eb5f49535b03ef0522a5f1d
Jiri Slaby <jslaby@suse.cz>
2015-03-19 11:38:14 +0100

    - mutex: Fix/document access-once assumption in   mutex_can_spin_on_owner() (bnc#923099). - Refresh   patches.fixes/0001-locking-mcs-Add-some-needed-ACCESS_ONCE-calls.patch.
    suse-commit: b63e425743eb3a356de0978623655a283be38622

--- kernel/mutex.c
+++ kernel/mutex.c
@@ -315,8 +315,8 @@ void osq_unlock(struct optimistic_spin_queue **lock)
  */
 static inline int mutex_can_spin_on_owner(struct mutex *lock)
 {
-	int retval = 1;
 	struct task_struct *owner;
+	int retval = 1;
 
 	if (need_resched())
 		return 0;
@@ -324,7 +324,7 @@ static inline int mutex_can_spin_on_owner(struct mutex *lock)
 	rcu_read_lock();
 	owner = ACCESS_ONCE(lock->owner);
 	if (owner)
-		retval = lock->owner->on_cpu;
+		retval = owner->on_cpu;
 	rcu_read_unlock();
 	/*
 	 * if lock->owner is not set, the mutex owner may have just acquired
