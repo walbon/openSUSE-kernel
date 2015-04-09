892789261614de1c54e186992da96df7eb64da88
Lee, Chun-Yi <jlee@suse.com>
2015-04-09 17:20:52 +0800

    i2c: i801: Add DeviceIDs for SunrisePoint LP (bnc#925554).
    suse-commit: 24f73db19b8803496653b26a82fe8cebb350767f

--- drivers/i2c/busses/i2c-i801.c
+++ drivers/i2c/busses/i2c-i801.c
@@ -61,6 +61,7 @@
   Coleto Creek (PCH)    0x23b0     32     hard     yes     yes     yes
   Wildcat Point-LP (PCH)   0x9ca2     32     hard     yes     yes     yes
   Sunrise Point-H (PCH)    0xa123     32     hard     yes     yes     yes
+  Sunrise Point-LP (PCH)   0x9d23     32     hard     yes     yes     yes
 
   Features supported by this driver:
   Software PEC                     no
@@ -165,6 +166,7 @@
 #define PCI_DEVICE_ID_INTEL_LYNXPOINT_LP_SMBUS	0x9c22
 #define PCI_DEVICE_ID_INTEL_WILDCATPOINT_LP_SMBUS	0x9ca2
 #define PCI_DEVICE_ID_INTEL_SUNRISEPOINT_H_SMBUS	0xa123
+#define PCI_DEVICE_ID_INTEL_SUNRISEPOINT_LP_SMBUS	0x9d23
 
 struct i801_priv {
 	struct i2c_adapter adapter;
@@ -663,6 +665,7 @@ static const struct pci_device_id i801_ids[] = {
 	{ PCI_DEVICE(PCI_VENDOR_ID_INTEL, PCI_DEVICE_ID_INTEL_COLETOCREEK_SMBUS) },
 	{ PCI_DEVICE(PCI_VENDOR_ID_INTEL, PCI_DEVICE_ID_INTEL_WILDCATPOINT_LP_SMBUS) },
 	{ PCI_DEVICE(PCI_VENDOR_ID_INTEL, PCI_DEVICE_ID_INTEL_SUNRISEPOINT_H_SMBUS) },
+	{ PCI_DEVICE(PCI_VENDOR_ID_INTEL, PCI_DEVICE_ID_INTEL_SUNRISEPOINT_LP_SMBUS) },
 	{ 0, }
 };
 
