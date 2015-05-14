25eb5d1bddc09ab5de194aca41e2aabaa4d3a776
Jan Kara <jack@suse.cz>
2015-05-14 13:21:15 +0200

    xfs: add XFS_IOC_FREE_EOFBLOCKS ioctl (bnc#930788).
    suse-commit: c5de538861ea11a2024b8c65b4aeab46df9ce94f

--- fs/xfs/linux-2.6/xfs_sync.h
+++ fs/xfs/linux-2.6/xfs_sync.h
@@ -51,7 +51,7 @@ void __xfs_inode_clear_reclaim_tag(struct xfs_mount *mp, struct xfs_perag *pag,
 
 void xfs_inode_set_eofblocks_tag(struct xfs_inode *ip);
 void xfs_inode_clear_eofblocks_tag(struct xfs_inode *ip);
-int xfs_icache_free_eofblocks(struct xfs_mount *, int);
+int xfs_icache_free_eofblocks(struct xfs_mount *, struct xfs_eofblocks *);
 
 int xfs_sync_inode_grab(struct xfs_inode *ip);
 int xfs_inode_ag_iterator(struct xfs_mount *mp,
