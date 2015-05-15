de604986b882bb2ba3d3d02cd7484f19c08f34f3
Hannes Reinecke <hare@suse.de>
2015-05-15 12:09:36 +0200

    Revert patch set 'dm-table-switch-to-readonly' (bsc#928801)
    - Refresh
      patches.fixes/dm-ignore-merge_bvec-for-snapshots-when-safe.patch.
    - Refresh patches.fixes/dm-optimize-use-SRCU-and-RCU.patch.
    - Delete patches.fixes/dm-release-map_lock-before-set_disk_ro.
    - Delete patches.fixes/dm-table-switch-to-readonly.
    
    suse-commit: b9af4d0d3d4621ef537b96544057231a810d53be

--- drivers/md/dm-table.c
+++ drivers/md/dm-table.c
@@ -462,18 +462,10 @@ int dm_get_device(struct dm_target *ti, const char *path, fmode_t mode,
 		dd->dm_dev.mode = mode;
 		dd->dm_dev.bdev = NULL;
 
-		r = open_dev(dd, dev, t->md);
-		if (r == -EROFS) {
-			dd->dm_dev.mode &= ~FMODE_WRITE;
-			r = open_dev(dd, dev, t->md);
-		}
-		if (r) {
+		if ((r = open_dev(dd, dev, t->md))) {
 			kfree(dd);
 			return r;
 		}
-
-		if (dd->dm_dev.mode != mode)
-			t->mode = dd->dm_dev.mode;
 
 		format_dev_t(dd->dm_dev.name, dev);
 
--- drivers/md/dm.c
+++ drivers/md/dm.c
@@ -347,25 +347,16 @@ int dm_deleting_md(struct mapped_device *md)
 static int dm_blk_open(struct block_device *bdev, fmode_t mode)
 {
 	struct mapped_device *md;
-	int retval = 0;
 
 	spin_lock(&_minor_lock);
 
 	md = bdev->bd_disk->private_data;
-	if (!md) {
-		retval = -ENXIO;
+	if (!md)
 		goto out;
-	}
 
 	if (test_bit(DMF_FREEING, &md->flags) ||
 	    dm_deleting_md(md)) {
 		md = NULL;
-		retval = -ENXIO;
-		goto out;
-	}
-	if (get_disk_ro(md->disk) && (mode & FMODE_WRITE)) {
-		md = NULL;
-		retval = -EROFS;
 		goto out;
 	}
 
@@ -375,7 +366,7 @@ static int dm_blk_open(struct block_device *bdev, fmode_t mode)
 out:
 	spin_unlock(&_minor_lock);
 
-	return retval;
+	return md ? 0 : -ENXIO;
 }
 
 static int dm_blk_close(struct gendisk *disk, fmode_t mode)
@@ -2197,12 +2188,6 @@ static struct dm_table *__bind(struct mapped_device *md, struct dm_table *t,
 		set_bit(DMF_MERGE_IS_OPTIONAL, &md->flags);
 	else
 		clear_bit(DMF_MERGE_IS_OPTIONAL, &md->flags);
-
-	if (!(dm_table_get_mode(t) & FMODE_WRITE)) {
-		set_disk_ro(md->disk, 1);
-	} else {
-		set_disk_ro(md->disk, 0);
-	}
 	dm_sync_table(md);
 
 	return old_map;
