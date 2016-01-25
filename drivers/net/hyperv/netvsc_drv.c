f9e1a73c26fd679fb9eb70109cda6d9a26b7be52
Olaf Hering <ohering@suse.de>
2016-01-25 12:09:22 +0100

    Delete obsolete hv_netvsc patch to handle RNDIS_STATUS_NETWORK_CHANGE
    suse-commit: f85f67a51ece7297de3a2e2dc8c3d093cbb0d537

--- drivers/net/hyperv/netvsc_drv.c
+++ drivers/net/hyperv/netvsc_drv.c
@@ -1023,7 +1023,7 @@ static void netvsc_link_change(struct work_struct *w)
 	struct netvsc_device *net_device;
 	struct rndis_device *rdev;
 	bool notify, refresh = false;
-	char *argv[] = { "/sbin/rcnetwork", "restart", NULL };
+	char *argv[] = { "/etc/init.d/network", "restart", NULL };
 	char *envp[] = { "HOME=/", "PATH=/sbin:/usr/sbin:/bin:/usr/bin", NULL };
 
 	rtnl_lock();
