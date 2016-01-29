cc59e80883519c84540de4413871c4c8d506745a
NeilBrown <neilb@suse.com>
2016-01-29 11:08:33 +1100

    Improve fairness when locking the per-superblock s_anon list (bsc#957525, bsc#941363).
    suse-commit: 44c10f3441cd81cd53ddee04c6260553f805fb23

--- fs/dcache.c
+++ fs/dcache.c
@@ -104,6 +104,8 @@ static unsigned int d_hash_shift __read_mostly;
 
 static struct hlist_bl_head *dentry_hashtable __read_mostly;
 
+static DEFINE_SPINLOCK(s_anon_lock);
+
 static inline struct hlist_bl_head *d_hash(const struct dentry *parent,
 					unsigned int hash)
 {
@@ -490,10 +492,14 @@ void __d_drop(struct dentry *dentry)
 		else
 			b = d_hash(dentry->d_parent, dentry->d_name.hash);
 
+		if (b == &dentry->d_sb->s_anon)
+			spin_lock(&s_anon_lock);
 		hlist_bl_lock(b);
 		__hlist_bl_del(&dentry->d_hash);
 		dentry->d_hash.pprev = NULL;
 		hlist_bl_unlock(b);
+		if (b == &dentry->d_sb->s_anon)
+			spin_unlock(&s_anon_lock);
 		dentry_rcuwalk_invalidate(dentry);
 	}
 }
@@ -1977,9 +1983,11 @@ static struct dentry *__d_obtain_alias(struct inode *inode, int disconnected)
 	spin_lock(&tmp->d_lock);
 	__d_set_inode_and_type(tmp, inode, add_flags);
 	hlist_add_head(&tmp->d_u.d_alias, &inode->i_dentry);
+	spin_lock(&s_anon_lock);
 	hlist_bl_lock(&tmp->d_sb->s_anon);
 	hlist_bl_add_head(&tmp->d_hash, &tmp->d_sb->s_anon);
 	hlist_bl_unlock(&tmp->d_sb->s_anon);
+	spin_unlock(&s_anon_lock);
 	spin_unlock(&tmp->d_lock);
 	spin_unlock(&inode->i_lock);
 	security_d_instantiate(tmp, inode);
