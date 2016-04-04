8b864775ddd9beb56cde4672f1067d1cd6ebeefc
Jeff Mahoney <jeffm@suse.com>
2016-04-04 10:38:19 -0400

    Delete patches.fixes/scsi-ibmvscsi-show-config.patch.
    This patch was added to address empty sysfs files in SLES9.  The root
    cause was ultimately identified later: the buffer size passed to the
    hardware to query was a 16-bit value, so 64k caused nothing to be
    returned.  It was fixed correctly in v3.7 commit 225c56960fc
    (ibmvscsi: Fix host config length field overflow), rendering this
    patch unnecessary.
    
    suse-commit: d1e631702445f1b308e68875dfcc11a764001cb9

--- drivers/scsi/ibmvscsi/ibmvscsi.c
+++ drivers/scsi/ibmvscsi/ibmvscsi.c
@@ -95,8 +95,6 @@ static int fast_fail = 1;
 static int client_reserve = 1;
 static char partition_name[97] = "UNKNOWN";
 static unsigned int partition_number = -1;
-/*host data buffer size*/
-#define buff_size 4096
 
 static struct scsi_transport_template *ibmvscsi_transport_template;
 
@@ -2003,7 +2001,7 @@ static ssize_t show_host_srp_version(struct device *dev,
 	struct ibmvscsi_host_data *hostdata = shost_priv(shost);
 	int len;
 
-       len = snprintf(buf, buff_size, "%s\n",
+	len = snprintf(buf, PAGE_SIZE, "%s\n",
 		       hostdata->madapter_info.srp_version);
 	return len;
 }
@@ -2024,7 +2022,7 @@ static ssize_t show_host_partition_name(struct device *dev,
 	struct ibmvscsi_host_data *hostdata = shost_priv(shost);
 	int len;
 
-       len = snprintf(buf, buff_size, "%s\n",
+	len = snprintf(buf, PAGE_SIZE, "%s\n",
 		       hostdata->madapter_info.partition_name);
 	return len;
 }
@@ -2045,7 +2043,7 @@ static ssize_t show_host_partition_number(struct device *dev,
 	struct ibmvscsi_host_data *hostdata = shost_priv(shost);
 	int len;
 
-       len = snprintf(buf, buff_size, "%d\n",
+	len = snprintf(buf, PAGE_SIZE, "%d\n",
 		       hostdata->madapter_info.partition_number);
 	return len;
 }
@@ -2065,7 +2063,7 @@ static ssize_t show_host_mad_version(struct device *dev,
 	struct ibmvscsi_host_data *hostdata = shost_priv(shost);
 	int len;
 
-       len = snprintf(buf, buff_size, "%d\n",
+	len = snprintf(buf, PAGE_SIZE, "%d\n",
 		       hostdata->madapter_info.mad_version);
 	return len;
 }
@@ -2085,7 +2083,7 @@ static ssize_t show_host_os_type(struct device *dev,
 	struct ibmvscsi_host_data *hostdata = shost_priv(shost);
 	int len;
 
-       len = snprintf(buf, buff_size, "%d\n", hostdata->madapter_info.os_type);
+	len = snprintf(buf, PAGE_SIZE, "%d\n", hostdata->madapter_info.os_type);
 	return len;
 }
 
@@ -2104,7 +2102,7 @@ static ssize_t show_host_config(struct device *dev,
 	struct ibmvscsi_host_data *hostdata = shost_priv(shost);
 
 	/* returns null-terminated host config data */
-       if (ibmvscsi_do_host_config(hostdata, buf, buff_size) == 0)
+	if (ibmvscsi_do_host_config(hostdata, buf, PAGE_SIZE) == 0)
 		return strlen(buf);
 	else
 		return 0;
