6fa2a00632785093962c0c5ec57de62eff00d461
Johannes Thumshirn <jthumshirn@suse.de>
2016-05-11 09:55:00 +0200

    nvme: fix max_segments integer truncation (bsc#676471).
    suse-commit: f0c27b86ae10d01f108360d65df0d09f3163388e

--- drivers/block/nvme-core.c
+++ drivers/block/nvme-core.c
@@ -2125,8 +2125,13 @@ static struct nvme_ns *nvme_alloc_ns(struct nvme_dev *dev, unsigned nsid,
 	ns->lba_shift = id->lbaf[lbaf].ds;
 	ns->ms = le16_to_cpu(id->lbaf[lbaf].ms);
 	blk_queue_logical_block_size(ns->queue, 1 << ns->lba_shift);
-	if (dev->max_hw_sectors)
+	if (dev->max_hw_sectors) {
+		u32 max_segments =
+			(dev->max_hw_sectors / (dev->page_size >> 9)) + 1;
 		blk_queue_max_hw_sectors(ns->queue, dev->max_hw_sectors);
+		blk_queue_max_segments(ns->queue,
+				       min_t(u32, max_segments, USHRT_MAX));
+	}
 	if (dev->vwc & NVME_CTRL_VWC_PRESENT)
 		blk_queue_flush(ns->queue, REQ_FLUSH | REQ_FUA);
 
@@ -2467,6 +2472,9 @@ static int nvme_dev_add(struct nvme_dev *dev)
 	memcpy(dev->firmware_rev, ctrl->fr, sizeof(ctrl->fr));
 	if (ctrl->mdts)
 		dev->max_hw_sectors = 1 << (ctrl->mdts + shift - 9);
+	else
+		dev->max_hw_sectors = UINT_MAX;
+
 	if ((pdev->vendor == PCI_VENDOR_ID_INTEL) &&
 			(pdev->device == 0x0953) && ctrl->vs[3])
 		dev->stripe_size = 1 << (ctrl->vs[3] + shift);
