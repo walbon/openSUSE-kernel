e2b1039c20716c69683bb8ea8030e86e2ad7fe28
NeilBrown <neilb@suse.com>
2016-05-18 13:59:46 +1000

    MM: increase safety margin provided by PF_LESS_THROTTLE (bsc#956491, FATE#320631).
    suse-commit: 24250a93eff63b4d99b83d1abd8861a950388090

--- mm/page-writeback.c
+++ mm/page-writeback.c
@@ -443,8 +443,8 @@ void global_dirty_limits(unsigned long *pbackground, unsigned long *pdirty)
 		background = dirty / 2;
 	tsk = current;
 	if (tsk->flags & PF_LESS_THROTTLE || rt_task(tsk)) {
-		background += background / 4;
-		dirty += dirty / 4;
+		background += background / 4 + vm_total_pages / 32;
+		dirty += dirty / 4 + vm_total_pages / 32;
 	}
 	*pbackground = background;
 	*pdirty = dirty;
