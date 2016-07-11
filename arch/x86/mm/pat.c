59ddb147d45eda91a9fb9236bdddef9f211d679f
Jiri Kosina <jkosina@suse.cz>
2016-07-11 16:55:46 +0200

    x86/mm/pat, /dev/mem: Remove superfluous error message (bsc#974620).
    suse-commit: 90cc9ecb7403c74d40c407ea44a21c3d4ea60394

--- arch/x86/mm/pat.c
+++ arch/x86/mm/pat.c
@@ -490,12 +490,8 @@ static inline int range_is_allowed(unsigned long pfn, unsigned long size)
 		return 1;
 
 	while (cursor < to) {
-		if (!devmem_is_allowed(pfn)) {
-			printk(KERN_INFO
-		"Program %s tried to access /dev/mem between %Lx->%Lx.\n",
-				current->comm, from, to);
+		if (!devmem_is_allowed(pfn))
 			return 0;
-		}
 		cursor += PAGE_SIZE;
 		pfn++;
 	}
