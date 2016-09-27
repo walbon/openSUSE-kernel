8dcf339eb4ed73940172d51d93a7fa93d30fbb24
Jan Beulich <jbeulich@suse.com>
2016-09-27 11:34:18 +0200

    blktap2: eliminate race from deferred work queue handling (bsc#911687).
    suse-commit: 6149a8947b89cedfae2acbdc98e08cea5a52d7e8

--- drivers/xen/blktap2/wait_queue.c
+++ drivers/xen/blktap2/wait_queue.c
@@ -15,13 +15,14 @@ blktap_run_deferred(void)
 
 	spin_lock_irqsave(&deferred_work_lock, flags);
 	list_splice_init(&deferred_work_queue, &queue);
-	list_for_each_entry(tap, &queue, deferred_queue)
-		clear_bit(BLKTAP_DEFERRED, &tap->dev_inuse);
 	spin_unlock_irqrestore(&deferred_work_lock, flags);
 
 	while (!list_empty(&queue)) {
 		tap = list_entry(queue.next, struct blktap, deferred_queue);
+		spin_lock_irqsave(&deferred_work_lock, flags);
+		clear_bit(BLKTAP_DEFERRED, &tap->dev_inuse);
 		list_del_init(&tap->deferred_queue);
+		spin_unlock_irqrestore(&deferred_work_lock, flags);
 		blktap_device_restart(tap);
 	}
 }
@@ -32,9 +33,7 @@ blktap_defer(struct blktap *tap)
 	unsigned long flags;
 
 	spin_lock_irqsave(&deferred_work_lock, flags);
-	if (!test_bit(BLKTAP_DEFERRED, &tap->dev_inuse)) {
-		set_bit(BLKTAP_DEFERRED, &tap->dev_inuse);
+	if (!test_and_set_bit(BLKTAP_DEFERRED, &tap->dev_inuse))
 		list_add_tail(&tap->deferred_queue, &deferred_work_queue);
-	}
 	spin_unlock_irqrestore(&deferred_work_lock, flags);
 }
