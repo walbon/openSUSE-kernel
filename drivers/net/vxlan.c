0f20dc4c2cdc3eca80196c78e161ab0e58c37e62
Michal Kubecek <mkubecek@suse.cz>
2016-10-10 01:06:22 +0200

    net: add recursion limit to GRO (CVE-2016-7039 bsc#1001486).
    suse-commit: 7a4036b256993b3aa85923277b09dd1c056ba823

--- drivers/net/vxlan.c
+++ drivers/net/vxlan.c
@@ -591,7 +591,7 @@ static struct sk_buff **vxlan_gro_receive(struct sk_buff **head,
 		}
 	}
 
-	pp = eth_gro_receive(head, skb);
+	pp = call_gro_receive(eth_gro_receive, head, skb);
 	flush = 0;
 
 out:
