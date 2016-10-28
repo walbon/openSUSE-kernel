a771c3bce9b8f5648bef6b268af2bcba0fd43a80
Coly Li <colyli@suse.de>
2016-10-28 18:31:54 +0800

    md: check command validity early in md_ioctl() (bsc#1004520).
    suse-commit: 64cc640ab4c375c3c8781c2a3daacd23d98ae6fa

--- drivers/md/md.c
+++ drivers/md/md.c
@@ -6206,6 +6206,32 @@ static int md_getgeo(struct block_device *bdev, struct hd_geometry *geo)
 	return 0;
 }
 
+static inline bool md_ioctl_valid(unsigned int cmd)
+{
+	switch (cmd) {
+	case ADD_NEW_DISK:
+	case BLKROSET:
+	case GET_ARRAY_INFO:
+	case GET_BITMAP_FILE:
+	case GET_DISK_INFO:
+	case HOT_ADD_DISK:
+	case HOT_REMOVE_DISK:
+	case PRINT_RAID_DEBUG:
+	case RAID_AUTORUN:
+	case RAID_VERSION:
+	case RESTART_ARRAY_RW:
+	case RUN_ARRAY:
+	case SET_ARRAY_INFO:
+	case SET_BITMAP_FILE:
+	case SET_DISK_FAULTY:
+	case STOP_ARRAY:
+	case STOP_ARRAY_RO:
+		return true;
+	default:
+		return false;
+	}
+}
+
 static int md_ioctl(struct block_device *bdev, fmode_t mode,
 			unsigned int cmd, unsigned long arg)
 {
@@ -6213,6 +6239,9 @@ static int md_ioctl(struct block_device *bdev, fmode_t mode,
 	void __user *argp = (void __user *)arg;
 	mddev_t *mddev = NULL;
 	int ro;
+
+	if (!md_ioctl_valid(cmd))
+		return -ENOTTY;
 
 	if (!capable(CAP_SYS_ADMIN))
 		return -EACCES;
