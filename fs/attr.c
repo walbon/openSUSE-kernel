fa0c39fd183e3db69345c13193eb0ff1b3e538c5
Jan Kara <jack@suse.cz>
2016-11-30 14:22:32 +0100

    fs: Avoid premature clearing of capabilities (bsc#914939 CVE-2015-1350).
    suse-commit: 422f015ac0789882919e881523c02bfd1d98af8b

--- fs/attr.c
+++ fs/attr.c
@@ -81,7 +81,18 @@ EXPORT_SYMBOL(inode_change_ok);
  */
 int setattr_prepare(struct dentry *dentry, struct iattr *attr)
 {
-	return inode_change_ok(dentry->d_inode, attr);
+	int error;
+
+	error = inode_change_ok(dentry->d_inode, attr);
+	if (error)
+		return error;
+	/* User has permission for the change */
+	if (attr->ia_valid & ATTR_KILL_PRIV) {
+		error = security_inode_killpriv(dentry);
+		attr->ia_valid &= ~ATTR_KILL_PRIV;
+	}
+	return error;
+
 }
 EXPORT_SYMBOL(setattr_prepare);
 
@@ -198,13 +209,11 @@ int notify_change(struct dentry * dentry, struct iattr * attr)
 	if (!(ia_valid & ATTR_MTIME_SET))
 		attr->ia_mtime = now;
 	if (ia_valid & ATTR_KILL_PRIV) {
-		attr->ia_valid &= ~ATTR_KILL_PRIV;
-		ia_valid &= ~ATTR_KILL_PRIV;
 		error = security_inode_need_killpriv(dentry);
-		if (error > 0)
-			error = security_inode_killpriv(dentry);
-		if (error)
+		if (error < 0)
 			return error;
+		if (error == 0)
+			ia_valid = attr->ia_valid &= ~ATTR_KILL_PRIV;
 	}
 
 	/*
@@ -251,8 +260,17 @@ int notify_change(struct dentry * dentry, struct iattr * attr)
 	if (ia_valid & ATTR_SIZE)
 		up_write(&dentry->d_inode->i_alloc_sem);
 
-	if (!error)
+	if (!error) {
+		/*
+		 * Backward compatibility hack for out of tree filesystems that
+		 * don't call setattr_prepare().
+		 */
+		if (attr->ia_valid & ATTR_KILL_PRIV) {
+			error = security_inode_killpriv(dentry);
+			attr->ia_valid &= ~ATTR_KILL_PRIV;
+		}
 		fsnotify_change(dentry, ia_valid);
+	}
 
 	return error;
 }
