1beee7a33ca6ddd8e8167d33776ca4e41d7550a6
Michal Kubecek <mkubecek@suse.cz>
2016-12-01 12:55:38 +0100

    tipc: check minimum bearer MTU (CVE-2016-8632 bsc#1008831).
    suse-commit: b29a190a8a3dbc998daa5e8560b6c7b0670f892c

--- net/tipc/udp_media.c
+++ net/tipc/udp_media.c
@@ -381,6 +381,11 @@ static int tipc_udp_enable(struct net *net, struct tipc_bearer *b,
 		udp_conf.local_ip.s_addr = htonl(INADDR_ANY);
 		udp_conf.use_udp_checksums = false;
 		ub->ifindex = dev->ifindex;
+		if (tipc_check_mtu(dev, sizeof(struct iphdr) +
+					sizeof(struct udphdr))) {
+			err = -EINVAL;
+			goto err;
+		}
 		b->mtu = dev->mtu - sizeof(struct iphdr)
 			- sizeof(struct udphdr);
 #if IS_ENABLED(CONFIG_IPV6)
