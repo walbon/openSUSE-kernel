fd6adb8189e2ed03faeef95dd541be0547b3474e
Joerg Roedel <jroedel@suse.de>
2016-12-16 16:00:13 +0100

    - mm/gup: Introduce get_user_pages_remote() (fate#321300). - Refresh   patches.drivers/drm-i915-userptr-Hold-mmref-whilst-calling-get-user.
    suse-commit: 6bce5429ec760f9a674ba30a8ee283c29ad96e07

--- drivers/gpu/drm/i915/i915_gem_userptr.c
+++ drivers/gpu/drm/i915/i915_gem_userptr.c
@@ -586,7 +586,7 @@ __i915_gem_userptr_get_pages_worker(struct work_struct *_work)
 		if (atomic_inc_not_zero(&mm->mm_users)) {
 			down_read(&mm->mmap_sem);
 			while (pinned < npages) {
-				ret = get_user_pages
+				ret = get_user_pages_remote
 					(work->task, mm,
 					 obj->userptr.ptr + pinned * PAGE_SIZE,
 					 npages - pinned,
