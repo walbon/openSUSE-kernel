a676f13917ff39cba0664d125830cd69aa547c92
Matthias Brugger <mbrugger@suse.com>
2016-12-21 16:57:52 +0100

    serial: 8250_dw: add ACPI support for uart on Hisilicon Hip05 SoC (fate#322061).
    suse-commit: 3e3a2017fc17383b24835adb9baa974547633ffe

--- drivers/tty/serial/8250/8250_dw.c
+++ drivers/tty/serial/8250/8250_dw.c
@@ -599,6 +599,7 @@ static const struct acpi_device_id dw8250_acpi_match[] = {
 	{ "8086228A", 0 },
 	{ "APMC0D08", 0},
 	{ "AMD0020", 0 },
+	{ "HISI0031", 0 },
 	{ },
 };
 MODULE_DEVICE_TABLE(acpi, dw8250_acpi_match);
