9766477598e527961dbf6418c63e788f2c6daae3
Jan Kara <jack@suse.cz>
2016-12-22 15:56:29 +0100

    - dax: export a low-level __dax_zero_page_range helper   (fate#321148). - Refresh   patches.drivers/dax-Use-radix-tree-entry-lock-to-protect-cow-faults.patch.
    suse-commit: e6ca1fa866d26f5aa0bf36c8ccc3a13be71c52f5

--- include/linux/dax.h
+++ include/linux/dax.h
@@ -22,6 +22,8 @@ void dax_wake_mapping_entry_waiter(struct address_space *mapping,
 #ifdef CONFIG_FS_DAX
 struct page *read_dax_sector(struct block_device *bdev, sector_t n);
 void dax_unlock_mapping_entry(struct address_space *mapping, pgoff_t index);
+int __dax_zero_page_range(struct block_device *bdev, sector_t sector,
+		unsigned int offset, unsigned int length);
 #else
 static inline struct page *read_dax_sector(struct block_device *bdev,
 		sector_t n)
@@ -33,6 +35,11 @@ static inline void dax_unlock_mapping_entry(struct address_space *mapping,
 					    pgoff_t index)
 {
 	BUG();
+}
+static inline int __dax_zero_page_range(struct block_device *bdev,
+		sector_t sector, unsigned int offset, unsigned int length)
+{
+	return -ENXIO;
 }
 #endif
 
