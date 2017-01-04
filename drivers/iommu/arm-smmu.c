b032239d8503d3e5c31b65e23a3aa46eb04a83ab
Mian Yousaf Kaukab <yousaf.kaukab@suse.com>
2017-01-04 17:46:03 +0100

    iommu/arm-smmu: Don't allocate resources for bypass domains (fate#320512, fate#322061).
    suse-commit: e3448a2af24986d9fe1d6b9db3ac05c2e0fcca11

--- drivers/iommu/arm-smmu.c
+++ drivers/iommu/arm-smmu.c
@@ -841,6 +841,12 @@ static int arm_smmu_init_domain_context(struct iommu_domain *domain,
 	if (smmu_domain->smmu)
 		goto out_unlock;
 
+	/* We're bypassing these SIDs, so don't allocate an actual context */
+	if (domain->type == IOMMU_DOMAIN_DMA) {
+		smmu_domain->smmu = smmu;
+		goto out_unlock;
+	}
+
 	/*
 	 * Mapping the requested stage onto what we support is surprisingly
 	 * complicated, mainly because the spec allows S1+S2 SMMUs without
@@ -963,7 +969,7 @@ static void arm_smmu_destroy_domain_context(struct iommu_domain *domain)
 	void __iomem *cb_base;
 	int irq;
 
-	if (!smmu)
+	if (!smmu || domain->type == IOMMU_DOMAIN_DMA)
 		return;
 
 	/*
