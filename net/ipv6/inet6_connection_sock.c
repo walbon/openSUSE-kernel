cbba4cfe536f751130d04dedbd16381cee0d9b53
Michal Kubecek <mkubecek@suse.cz>
2017-01-26 13:20:53 +0100

    tcp: fix inet6_csk_route_req() for link-local addresses (bsc#1010175).
    suse-commit: 488054375d4f45e60b09880033a241c047437466

--- net/ipv6/inet6_connection_sock.c
+++ net/ipv6/inet6_connection_sock.c
@@ -76,7 +76,7 @@ struct dst_entry *inet6_csk_route_req(struct sock *sk,
 	final_p = fl6_update_dst(&fl6, rcu_dereference(np->opt), &final);
 	rcu_read_unlock();
 	ipv6_addr_copy(&fl6.saddr, &treq->loc_addr);
-	fl6.flowi6_oif = sk->sk_bound_dev_if;
+	fl6.flowi6_oif = treq->iif;
 	fl6.flowi6_mark = sk->sk_mark;
 	fl6.fl6_dport = inet_rsk(req)->rmt_port;
 	fl6.fl6_sport = inet_rsk(req)->loc_port;
