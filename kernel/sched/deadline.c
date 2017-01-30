2a37804f0a054ec0789e85a1548665dbfc55a17c
Mike Galbraith <mgalbraith@suse.de>
2017-01-30 11:36:14 +0100

    sched/deadline: Fix lock pinning warning during CPU hotplug (bnc#1022476).
    suse-commit: 9052c61dee1dd9a7118db7bd6d1243791ff17981

--- kernel/sched/deadline.c
+++ kernel/sched/deadline.c
@@ -670,8 +670,11 @@ static enum hrtimer_restart dl_task_timer(struct hrtimer *timer)
 	 *
 	 * XXX figure out if select_task_rq_dl() deals with offline cpus.
 	 */
-	if (unlikely(!rq->online))
+	if (unlikely(!rq->online)) {
+		lockdep_unpin_lock(&rq->lock);
 		rq = dl_task_offline_migration(rq, p);
+		lockdep_pin_lock(&rq->lock);
+	}
 
 	/*
 	 * Queueing this task back might have overloaded rq, check if we need
