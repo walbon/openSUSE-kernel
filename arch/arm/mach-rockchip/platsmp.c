75a369915b2eb61464b5671cf632ccc94ba77224
Matthias Brugger <mbrugger@suse.com>
2017-02-10 17:46:28 +0100

    ARM: rockchip: Fix use of plain integer as NULL pointer (fate#322003).
    suse-commit: 65de69e1e9938b5a8316f80698e03555c7eaf252

--- arch/arm/mach-rockchip/platsmp.c
+++ arch/arm/mach-rockchip/platsmp.c
@@ -64,7 +64,7 @@ static struct reset_control *rockchip_get_core_reset(int cpu)
 	if (dev)
 		np = dev->of_node;
 	else
-		np = of_get_cpu_node(cpu, 0);
+		np = of_get_cpu_node(cpu, NULL);
 
 	return of_reset_control_get(np, NULL);
 }
