53ea5dbd3052e81aedeffd6468e993433ae4b1a0
Matthias Brugger <mbrugger@suse.com>
2017-02-10 17:46:28 +0100

    cpufreq: dt: Kill platform-data (fate#322003).
    suse-commit: 5bfbf3a881040a73a4cb88784c8b6179ce5c085f

--- drivers/cpufreq/cpufreq-dt.c
+++ drivers/cpufreq/cpufreq-dt.c
@@ -18,7 +18,6 @@
 #include <linux/cpu.h>
 #include <linux/cpu_cooling.h>
 #include <linux/cpufreq.h>
-#include <linux/cpufreq-dt.h>
 #include <linux/cpumask.h>
 #include <linux/err.h>
 #include <linux/module.h>
@@ -252,10 +251,7 @@ static int cpufreq_init(struct cpufreq_policy *policy)
 	}
 
 	if (need_update) {
-		struct cpufreq_dt_platform_data *pd = cpufreq_get_driver_data();
-
-		if (!pd || !pd->independent_clocks)
-			cpumask_setall(policy->cpus);
+		cpumask_setall(policy->cpus);
 
 		/*
 		 * OPP tables are initialized only for policy->cpu, do it for
