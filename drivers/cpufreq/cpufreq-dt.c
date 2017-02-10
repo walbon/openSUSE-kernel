09052e2c6e2e9cb45aa5a8c2ca579a7bcc30b354
Matthias Brugger <mbrugger@suse.com>
2017-02-10 17:46:28 +0100

    cpufreq: dt: Support governor tunables per policy (fate#322003).
    suse-commit: 1eed81d519274348c2372ea876a920d4323f9c27

--- drivers/cpufreq/cpufreq-dt.c
+++ drivers/cpufreq/cpufreq-dt.c
@@ -28,6 +28,8 @@
 #include <linux/slab.h>
 #include <linux/thermal.h>
 
+#include "cpufreq-dt.h"
+
 struct private_data {
 	struct device *cpu_dev;
 	struct regulator *cpu_reg;
@@ -435,6 +437,7 @@ static int dt_cpufreq_probe(struct platform_device *pdev)
 	struct device *cpu_dev;
 	struct regulator *cpu_reg;
 	struct clk *cpu_clk;
+	struct cpufreq_dt_platform_data *data = dev_get_platdata(&pdev->dev);
 	int ret;
 
 	/*
@@ -453,6 +456,9 @@ static int dt_cpufreq_probe(struct platform_device *pdev)
 		regulator_put(cpu_reg);
 
 	dt_cpufreq_driver.driver_data = dev_get_platdata(&pdev->dev);
+
+	if (data && data->have_governor_per_policy)
+		dt_cpufreq_driver.flags |= CPUFREQ_HAVE_GOVERNOR_PER_POLICY;
 
 	ret = cpufreq_register_driver(&dt_cpufreq_driver);
 	if (ret)
