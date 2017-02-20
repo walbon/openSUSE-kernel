4a331fa743098221e101d9dec3b9690589ee4625
Mian Yousaf Kaukab <yousaf.kaukab@suse.com>
2017-02-20 21:30:30 +0100

    arm64: Add ARM64_HYP_OFFSET_LOW capability (fate#320512).
    suse-commit: f578ae724a245325de0a9fa0c998447921cdb760

--- arch/arm64/include/asm/cpufeature.h
+++ arch/arm64/include/asm/cpufeature.h
@@ -37,8 +37,9 @@
 #define ARM64_ALT_PAN_NOT_UAO			12
 #define ARM64_HAS_32BIT_EL0			13
 #define ARM64_HAS_VIRT_HOST_EXTN		14
+#define ARM64_HYP_OFFSET_LOW			15
 
-#define ARM64_NCAPS				15
+#define ARM64_NCAPS				16
 
 #ifndef __ASSEMBLY__
 
