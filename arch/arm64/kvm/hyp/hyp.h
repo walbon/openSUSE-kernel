6b8eb1397ad9702a775f8dd9a99dd9f12eacebc1
Mian Yousaf Kaukab <yousaf.kaukab@suse.com>
2017-02-20 21:30:30 +0100

    arm64: KVM: Implement timer save/restore (fate#320512).
    suse-commit: 6882a325023db369379900118c0934aaa30cd1c0

--- arch/arm64/kvm/hyp/hyp.h
+++ arch/arm64/kvm/hyp/hyp.h
@@ -35,4 +35,7 @@ void __vgic_v2_restore_state(struct kvm_vcpu *vcpu);
 void __vgic_v3_save_state(struct kvm_vcpu *vcpu);
 void __vgic_v3_restore_state(struct kvm_vcpu *vcpu);
 
+void __timer_save_state(struct kvm_vcpu *vcpu);
+void __timer_restore_state(struct kvm_vcpu *vcpu);
+
 #endif /* __ARM64_KVM_HYP_H__ */
