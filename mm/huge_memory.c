9607384fbf2f68cd6210f9bf95d5bcf837195faa
Michal Hocko <mhocko@suse.cz>
2017-03-01 10:51:45 +0100

    mm: don't assume anonymous pages have SwapBacked flag (bnc#1027383).
    suse-commit: ca20177c8b125b5d970f606c894e870ab5321975

--- mm/huge_memory.c
+++ mm/huge_memory.c
@@ -2059,7 +2059,6 @@ int split_huge_page_to_list(struct page *page, struct list_head *list)
 	if (!PageCompound(page))
 		goto out_unlock;
 
-	BUG_ON(!PageSwapBacked(page));
 	__split_huge_page(page, anon_vma, list);
 	count_vm_event(THP_SPLIT);
 
@@ -2260,7 +2259,7 @@ void __khugepaged_exit(struct mm_struct *mm)
 static void release_pte_page(struct page *page)
 {
 	/* 0 stands for page_is_file_cache(page) == false */
-	dec_zone_page_state(page, NR_ISOLATED_ANON + 0);
+	dec_zone_page_state(page, NR_ISOLATED_ANON + page_is_file_cache(page));
 	unlock_page(page);
 	putback_lru_page(page);
 }
@@ -2301,7 +2300,6 @@ static int __collapse_huge_page_isolate(struct vm_area_struct *vma,
 
 		VM_BUG_ON_PAGE(PageCompound(page), page);
 		VM_BUG_ON_PAGE(!PageAnon(page), page);
-		VM_BUG_ON_PAGE(!PageSwapBacked(page), page);
 
 		/*
 		 * We can do it before isolate_lru_page because the
@@ -2343,7 +2341,7 @@ static int __collapse_huge_page_isolate(struct vm_area_struct *vma,
 			goto out;
 		}
 		/* 0 stands for page_is_file_cache(page) == false */
-		inc_zone_page_state(page, NR_ISOLATED_ANON + 0);
+		inc_zone_page_state(page, NR_ISOLATED_ANON + page_is_file_cache(page));
 		VM_BUG_ON_PAGE(!PageLocked(page), page);
 		VM_BUG_ON_PAGE(PageLRU(page), page);
 
