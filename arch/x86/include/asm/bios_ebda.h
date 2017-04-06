6b93f95c7f55f527685fc08b679a09089b630f1a
Matthias Brugger <mbrugger@suse.com>
2017-04-06 19:12:39 +0200

    - x86: boot: rename reserve_bios_regions (bsc#1031345). - Delete   patches.arch/qcom-0019-x86-rtc-Replace-paravirt-rtc-check-with-platform-leg.patch. - Delete   patches.arch/qcom-0020-x86-init-Use-a-platform-legacy-quirk-for-EBDA.patch. - Delete   patches.arch/qcom-0021-x86-init-Rename-EBDA-code-file.patch. - Delete   patches.arch/qcom-0029-x86-boot-Reorganize-and-clean-up-the-BIOS-area-reser.patch.
    suse-commit: 30d51ff33fc7a987f6459034d29b1799ee716eec

--- arch/x86/include/asm/bios_ebda.h
+++ arch/x86/include/asm/bios_ebda.h
@@ -38,7 +38,7 @@ static inline unsigned int get_bios_ebda_length(void)
 	return length;
 }
 
-void reserve_bios_regions(void);
+void reserve_ebda_region(void);
 
 #ifdef CONFIG_X86_CHECK_BIOS_CORRUPTION
 /*
--- arch/x86/kernel/rtc.c
+++ arch/x86/kernel/rtc.c
@@ -14,7 +14,6 @@
 #include <asm/time.h>
 #include <asm/intel-mid.h>
 #include <asm/rtc.h>
-#include <asm/setup.h>
 
 #ifdef CONFIG_X86_32
 /*
@@ -189,6 +188,10 @@ static __init int add_rtc_cmos(void)
 	if (of_have_populated_dt())
 		return 0;
 
+	/* Intel MID platforms don't have ioport rtc */
+	if (intel_mid_identify_cpu())
+		return -ENODEV;
+
 #ifdef CONFIG_ACPI
 	if (acpi_gbl_FADT.boot_flags & ACPI_FADT_NO_CMOS_RTC) {
 		/* This warning can likely go away again in a year or two. */
@@ -197,7 +200,7 @@ static __init int add_rtc_cmos(void)
 	}
 #endif
 
-	if (!x86_platform.legacy.rtc)
+	if (paravirt_enabled() && !paravirt_has(RTC))
 		return -ENODEV;
 
 	platform_device_register(&rtc_device);
