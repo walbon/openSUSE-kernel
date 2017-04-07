e077947216d21f8d3f91e5b5ca276b3806db9430
Thomas Bogendoerfer <tbogendoerfer@suse.de>
2017-04-07 14:13:08 +0200

    IB/core: Support rate limit for packet pacing (FATE#321231 FATE#321473 FATE#322153 FATE#322149).
    suse-commit: 9af2fddd5a934047ce9ef0ea042db4fa7b6cf616

--- drivers/infiniband/core/verbs.c
+++ drivers/infiniband/core/verbs.c
@@ -1011,6 +1011,7 @@ static const struct {
 						 IB_QP_QKEY),
 				 [IB_QPT_GSI] = (IB_QP_CUR_STATE		|
 						 IB_QP_QKEY),
+				 [IB_QPT_RAW_PACKET] = IB_QP_RATE_LIMIT,
 			 }
 		}
 	},
@@ -1044,6 +1045,7 @@ static const struct {
 						IB_QP_QKEY),
 				[IB_QPT_GSI] = (IB_QP_CUR_STATE			|
 						IB_QP_QKEY),
+				[IB_QPT_RAW_PACKET] = IB_QP_RATE_LIMIT,
 			}
 		},
 		[IB_QPS_SQD]   = {
