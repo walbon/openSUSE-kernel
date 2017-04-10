fd5e73a6d761e6cdca56bb57e94ff189032de9bb
Matthias Brugger <mbrugger@suse.com>
2017-04-10 16:53:04 +0200

    mmc: dw_mmc: force setup bus if active slots exist (bsc#1033273).
    suse-commit: f12782df7f47d08c219c30b72bf9b615ab1351ba

--- drivers/mmc/host/dw_mmc.c
+++ drivers/mmc/host/dw_mmc.c
@@ -3262,10 +3262,11 @@ int dw_mci_resume(struct dw_mci *host)
 
 		if (!slot)
 			continue;
-		if (slot->mmc->pm_flags & MMC_PM_KEEP_POWER) {
+		if (slot->mmc->pm_flags & MMC_PM_KEEP_POWER)
 			dw_mci_set_ios(slot->mmc, &slot->mmc->ios);
-			dw_mci_setup_bus(slot, true);
-		}
+
+		/* Force setup bus to guarantee available clock output */
+		dw_mci_setup_bus(slot, true);
 	}
 
 	/* Now that slots are all setup, we can enable card detect */
