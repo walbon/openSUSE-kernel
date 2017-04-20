9e2244c6c7f4ff0a1b1aed006078b5f911d3733d
David Sterba <dsterba@suse.com>
2017-04-20 16:38:09 +0200

    Btrfs: Clean pte corresponding to page straddling i_size (bsc#1012452).
    suse-commit: 0abe380f63829c6b683b6f4b180594d781f8dad2

--- fs/btrfs/inode.c
+++ fs/btrfs/inode.c
@@ -4942,7 +4942,6 @@ static int btrfs_setsize(struct inode *inode, struct iattr *attr)
 	}
 
 	if (newsize > oldsize) {
-		truncate_pagecache(inode, newsize);
 		/*
 		 * Don't do an expanding truncate while snapshoting is ongoing.
 		 * This is to ensure the snapshot captures a fully consistent
@@ -4965,6 +4964,7 @@ static int btrfs_setsize(struct inode *inode, struct iattr *attr)
 
 		i_size_write(inode, newsize);
 		btrfs_ordered_update_i_size(inode, i_size_read(inode), NULL);
+		pagecache_isize_extended(inode, oldsize, newsize);
 		ret = btrfs_update_inode(trans, root, inode);
 		btrfs_end_write_no_snapshoting(root);
 		btrfs_end_transaction(trans, root);
