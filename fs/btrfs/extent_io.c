61b80925ea9f51b64d165e9e95e22245ab1201ab
David Sterba <dsterba@suse.com>
2017-04-21 18:44:04 +0200

    btrfs: untangle gotos a bit in __clear_extent_bit (bsc#1012452).
    suse-commit: 42a71ce78d7b8955c48cbe0f4426620f1ba521f8

--- fs/btrfs/extent_io.c
+++ fs/btrfs/extent_io.c
@@ -728,14 +728,6 @@ next:
 	start = last_end + 1;
 	if (start <= end && state && !need_resched())
 		goto hit_next;
-	goto search_again;
-
-out:
-	spin_unlock(&tree->lock);
-	if (prealloc)
-		free_extent_state(prealloc);
-
-	return 0;
 
 search_again:
 	if (start > end)
@@ -744,6 +736,14 @@ search_again:
 	if (gfpflags_allow_blocking(mask))
 		cond_resched();
 	goto again;
+
+out:
+	spin_unlock(&tree->lock);
+	if (prealloc)
+		free_extent_state(prealloc);
+
+	return 0;
+
 }
 
 static void wait_on_state(struct extent_io_tree *tree,
