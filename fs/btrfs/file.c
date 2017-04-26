52935934eb8ad69f6bff9026f0b99748ce7b596d
David Sterba <dsterba@suse.com>
2017-04-26 13:33:44 +0200

    Btrfs: fix callers of btrfs_block_rsv_migrate (bsc#1012452).
    suse-commit: cf3c74d8473e8c7c9c26f42f575987624d39754c

--- fs/btrfs/file.c
+++ fs/btrfs/file.c
@@ -2482,7 +2482,7 @@ static int btrfs_punch_hole(struct inode *inode, loff_t offset, loff_t len)
 	}
 
 	ret = btrfs_block_rsv_migrate(&root->fs_info->trans_block_rsv, rsv,
-				      min_size);
+				      min_size, 0);
 	BUG_ON(ret);
 	trans->block_rsv = rsv;
 
@@ -2525,7 +2525,7 @@ static int btrfs_punch_hole(struct inode *inode, loff_t offset, loff_t len)
 		}
 
 		ret = btrfs_block_rsv_migrate(&root->fs_info->trans_block_rsv,
-					      rsv, min_size);
+					      rsv, min_size, 0);
 		BUG_ON(ret);	/* shouldn't happen */
 		trans->block_rsv = rsv;
 
