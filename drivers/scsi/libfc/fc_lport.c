220c6c38007772dd1e0a1a414cf25c3f2690ab66
Hannes Reinecke <hare@suse.de>
2017-04-27 15:51:54 +0200

    scsi: fc: provide fc_bsg_to_rport() helper (bsc#1019675,FATE#321701).
    suse-commit: a4ddd4feac458b597e8fe3cf9b0a2bdd982d0c60

--- drivers/scsi/libfc/fc_lport.c
+++ drivers/scsi/libfc/fc_lport.c
@@ -2106,7 +2106,7 @@ int fc_lport_bsg_request(struct fc_bsg_job *job)
 
 	switch (bsg_request->msgcode) {
 	case FC_BSG_RPT_ELS:
-		rport = job->rport;
+		rport = fc_bsg_to_rport(job);
 		if (!rport)
 			break;
 
@@ -2116,7 +2116,7 @@ int fc_lport_bsg_request(struct fc_bsg_job *job)
 		break;
 
 	case FC_BSG_RPT_CT:
-		rport = job->rport;
+		rport = fc_bsg_to_rport(job);
 		if (!rport)
 			break;
 
