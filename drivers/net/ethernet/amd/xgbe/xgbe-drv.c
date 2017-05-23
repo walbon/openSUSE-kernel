2f805b1f97eb305d7eeb7ce4a9fda1a373d17435
Alexander Graf <agraf@suse.de>
2017-05-23 16:12:39 +0200

    amd-xgbe: Check xgbe_init() return code (bsc#1040392).
    suse-commit: ef2a532c3d103647ebfc8c0907836afa5cd81c4f

--- drivers/net/ethernet/amd/xgbe/xgbe-drv.c
+++ drivers/net/ethernet/amd/xgbe/xgbe-drv.c
@@ -1071,7 +1071,9 @@ static int xgbe_start(struct xgbe_prv_data *pdata)
 
 	DBGPR("-->xgbe_start\n");
 
-	hw_if->init(pdata);
+	ret = hw_if->init(pdata);
+	if (ret)
+		return ret;
 
 	xgbe_napi_enable(pdata, 1);
 
