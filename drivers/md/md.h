dd0f501269a7881098f48a3b21ab479edc9995e9
Takashi Iwai <tiwai@suse.de>
2017-06-23 20:47:07 +0200

    Delete patches.kabi/0001-md-use-a-separate-bio_set-for-synchronous-IO.kabi.
    Not needed yet for Leap kernel.
    
    suse-commit: 4e6675abaa5141b27208d01f71b4927bf67f86a5

--- drivers/md/md.h
+++ drivers/md/md.h
@@ -446,6 +446,9 @@ struct mddev {
 	struct attribute_group		*to_remove;
 
 	struct bio_set			*bio_set;
+	struct bio_set			*sync_set; /* for sync operations like
+						   * metadata and bitmap writes
+						   */
 
 	/* Generic flush handling.
 	 * The last to finish preflush schedules a worker to submit
@@ -458,11 +461,6 @@ struct mddev {
 	void (*sync_super)(struct mddev *mddev, struct md_rdev *rdev);
 	struct md_cluster_info		*cluster_info;
 	unsigned int			good_device_nr;	/* good device num within cluster raid */
-#ifndef __GENKSYMS__
-	struct bio_set			*sync_set; /* for sync operations like
-						   * metadata and bitmap writes
-						   */
-#endif
 };
 
 enum recovery_flags {
