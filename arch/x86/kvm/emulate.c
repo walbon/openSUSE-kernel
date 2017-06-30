98779c1ac76336808c18728a343c6ec6e655d36e
Takashi Iwai <tiwai@suse.de>
2017-06-30 21:11:19 +0200

    Delete patches.kabi/kabi-fix-for-kvm-x86-fix-singlestepping-over-syscall.
    No need for kABI fix yet for Leap 42.3.
    
    suse-commit: f0a86d9664bafcb94e27e42d2f2f49fb0b3a77b8

--- arch/x86/kvm/emulate.c
+++ arch/x86/kvm/emulate.c
@@ -2726,9 +2726,7 @@ static int em_syscall(struct x86_emulate_ctxt *ctxt)
 		ctxt->eflags &= ~(X86_EFLAGS_VM | X86_EFLAGS_IF);
 	}
 
-	if ((ctxt->eflags & X86_EFLAGS_TF) != 0)
-		ctxt->emul_flags |= X86EMUL_TF_BEFORE_INST;
-
+	ctxt->tf = (ctxt->eflags & X86_EFLAGS_TF) != 0;
 	return X86EMUL_CONTINUE;
 }
 
