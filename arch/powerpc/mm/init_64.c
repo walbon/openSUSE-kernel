e4ec7f96d2c312401fbd6df1e3cd770f24f935c4
Michal Suchanek <msuchanek@suse.de>
2017-07-14 20:35:35 +0200

    powerpc/mm: Remove checks that TASK_SIZE_USER64 is too small   (fate#322495, bsc#1032471).
    suse-commit: dd08acdc85aea934f597999233f76eecd0c5fde0

--- arch/powerpc/mm/init_64.c
+++ arch/powerpc/mm/init_64.c
@@ -72,9 +72,11 @@
 #if PGTABLE_RANGE > USER_VSID_RANGE
 #warning Limited user VSID range means pagetable space is wasted
 #endif
+#ifndef CONFIG_BIGMEM
 
 #if (TASK_SIZE_USER64 < PGTABLE_RANGE) && (TASK_SIZE_USER64 < USER_VSID_RANGE)
 #warning TASK_SIZE is smaller than it needs to be.
+#endif
 #endif
 #endif /* CONFIG_PPC_STD_MMU_64 */
 
