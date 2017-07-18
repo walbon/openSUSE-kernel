efa0e82d80bd1b2b736c6af0cf4c4991d2f9540d
Juergen Gross <jgross@suse.com>
2017-07-18 11:23:31 +0200

    xen/pvh*: Support > 32 VCPUs at domain restore (bnc#1045563).
    suse-commit: e9e458ba2aec436a710728aaaae34e65478a6051

--- arch/x86/xen/suspend.c
+++ arch/x86/xen/suspend.c
@@ -33,16 +33,11 @@ static void xen_pv_pre_suspend(void)
 static void xen_hvm_post_suspend(int suspend_cancelled)
 {
 #ifdef CONFIG_XEN_PVHVM
-	int cpu;
 	if (!suspend_cancelled)
-	    xen_hvm_init_shared_info();
+		xen_hvm_init_shared_info();
 	xen_callback_vector();
 	xen_unplug_emulated_devices();
-	if (xen_feature(XENFEAT_hvm_safe_pvclock)) {
-		for_each_online_cpu(cpu) {
-			xen_setup_runstate_info(cpu);
-		}
-	}
+	xen_vcpu_restore();
 #endif
 }
 
