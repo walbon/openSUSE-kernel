32479171844507cba40ac8b99971541448a225b5
Luis R. Rodriguez <mcgrof@suse.com>
2017-07-21 14:57:19 -0700

    sysctl: add unsigned int range support (FATE#323821)
    suse-commit: ff03c6d7b876a4fbbe7b7f8ff00edbb15e87c36b

--- fs/proc/proc_sysctl.c
+++ fs/proc/proc_sysctl.c
@@ -1029,7 +1029,8 @@ static int sysctl_check_table_array(const char *path, struct ctl_table *table)
 {
 	int err = 0;
 
-	if (table->proc_handler == proc_douintvec) {
+	if ((table->proc_handler == proc_douintvec) ||
+	    (table->proc_handler == proc_douintvec_minmax)) {
 		if (table->maxlen != sizeof(unsigned int))
 			err |= sysctl_err(path, table, "array now allowed");
 	}
@@ -1046,6 +1047,7 @@ static int sysctl_check_table(const char *path, struct ctl_table *table)
 
 		if ((table->proc_handler == proc_dostring) ||
 		    (table->proc_handler == proc_dointvec) ||
+		    (table->proc_handler == proc_douintvec_minmax) ||
 		    (table->proc_handler == proc_dointvec_minmax) ||
 		    (table->proc_handler == proc_dointvec_jiffies) ||
 		    (table->proc_handler == proc_dointvec_userhz_jiffies) ||
