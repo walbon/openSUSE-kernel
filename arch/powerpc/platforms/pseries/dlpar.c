f077380454ffc1494a00ddd7e43a65f50fe35d80
Michal Suchanek <msuchanek@suse.de>
2017-11-14 14:26:53 +0100

    powerpc/hotplug: Improve responsiveness of hotplug change (FATE#322022, bsc#1067906).
    suse-commit: 33b963e5784968bf38a0c322b6623cd705ceed4a

--- arch/powerpc/platforms/pseries/dlpar.c
+++ arch/powerpc/platforms/pseries/dlpar.c
@@ -371,6 +371,7 @@ static int dlpar_online_cpu(struct device_node *dn)
 			BUG_ON(get_cpu_current_state(cpu)
 					!= CPU_STATE_OFFLINE);
 			cpu_maps_update_done();
+			timed_topology_update(1);
 			rc = device_online(get_cpu_device(cpu));
 			if (rc)
 				goto out;
@@ -490,6 +491,7 @@ static int dlpar_offline_cpu(struct device_node *dn)
 			if (get_cpu_current_state(cpu) == CPU_STATE_ONLINE) {
 				set_preferred_offline_state(cpu, CPU_STATE_OFFLINE);
 				cpu_maps_update_done();
+				timed_topology_update(1);
 				rc = device_offline(get_cpu_device(cpu));
 				if (rc)
 					goto out;
