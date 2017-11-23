cb21698965f70f7ab5aa047a2da8c7fff811ef18
Takashi Iwai <tiwai@suse.de>
2017-11-23 16:36:06 +0100

    drm/amdkfd: NULL dereference involving create_process() (bsc#1031717).
    suse-commit: 642e15144d72f0513b89eae90e79671b5cae87d3

--- drivers/gpu/drm/amd/amdkfd/kfd_process.c
+++ drivers/gpu/drm/amd/amdkfd/kfd_process.c
@@ -318,7 +318,8 @@ static struct kfd_process *create_process(const struct task_struct *thread)
 
 	/* init process apertures*/
 	process->is_32bit_user_mode = is_compat_task();
-	if (kfd_init_apertures(process) != 0)
+	err = kfd_init_apertures(process);
+	if (err != 0)
 		goto err_init_apretures;
 
 	return process;
