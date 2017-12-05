8ee731e735d3b5cb1c1b628a091e17e6b82ac8a2
Takashi Iwai <tiwai@suse.de>
2017-12-05 12:39:40 +0100

    KEYS: fix key refcount leak in keyctl_assume_authority() (bsc#1031717).
    suse-commit: 6e58fbf968852e9f729616bf4cae461fcf653b11

--- security/keys/keyctl.c
+++ security/keys/keyctl.c
@@ -1380,11 +1380,9 @@ long keyctl_assume_authority(key_serial_t id)
 	}
 
 	ret = keyctl_change_reqkey_auth(authkey);
-	if (ret < 0)
-		goto error;
+	if (ret == 0)
+		ret = authkey->serial;
 	key_put(authkey);
-
-	ret = authkey->serial;
 error:
 	return ret;
 }
