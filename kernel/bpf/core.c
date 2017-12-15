c615b60d0a0aa2bf7af1d54b9654ad87b9d33b01
Jiri Slaby <jslaby@suse.cz>
2017-12-15 11:38:35 +0100

    bpf: prevent speculative execution in eBPF interpreter (bnc#1068032).
    suse-commit: bfa26bb5bd3e2a0845806931617f0547042e6d3a

--- kernel/bpf/core.c
+++ kernel/bpf/core.c
@@ -29,6 +29,7 @@
 #include <linux/bpf.h>
 
 #include <asm/unaligned.h>
+#include <asm/barrier.h>
 
 /* Registers */
 #define BPF_R0	regs[BPF_REG_0]
@@ -356,6 +357,7 @@ select_insn:
 		DST = IMM;
 		CONT;
 	LD_IMM_DW:
+		gmb();
 		DST = (u64) (u32) insn[0].imm | ((u64) (u32) insn[1].imm) << 32;
 		insn++;
 		CONT;
@@ -570,6 +572,7 @@ out:
 		*(SIZE *)(unsigned long) (DST + insn->off) = IMM;	\
 		CONT;							\
 	LDX_MEM_##SIZEOP:						\
+		gmb();							\
 		DST = *(SIZE *)(unsigned long) (SRC + insn->off);	\
 		CONT;
 
