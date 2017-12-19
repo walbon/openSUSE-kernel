c2773f8cc311c63801e279ed085bf71e98e8af91
Borislav Petkov <bp@suse.de>
2017-12-19 17:27:34 +0100

    x86/kvm: Toggle IBRS on VM entry and exit (bsc#1068032).
    suse-commit: 8773ba25bbedd0ddb18cf295408a36d9016a025f

--- arch/x86/kvm/vmx.c
+++ arch/x86/kvm/vmx.c
@@ -8650,6 +8650,11 @@ static void __noclone vmx_vcpu_run(struct kvm_vcpu *vcpu)
 		__write_pkru(vmx->guest_pkru);
 
 	atomic_switch_perf_msrs(vmx);
+
+	if (boot_cpu_has(X86_FEATURE_SPEC_CTRL))
+		add_atomic_switch_msr(vmx, MSR_IA32_SPEC_CTRL,
+			vcpu->arch.spec_ctrl, FEATURE_ENABLE_IBRS);
+
 	debugctlmsr = get_debugctlmsr();
 
 	vmx->__launched = vmx->loaded_vmcs->launched;
