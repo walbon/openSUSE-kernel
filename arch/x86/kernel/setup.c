13dae54cb229d078635f159dd8afe16ae683980b
Borislav Petkov <bp@suse.de>
2017-12-25 14:10:46 +0100

    x86/kaiser: Move feature detection up (bsc#1068032).
    suse-commit: 399eb34b7a8a27f8c20e267eefc616f259243670

--- arch/x86/kernel/setup.c
+++ arch/x86/kernel/setup.c
@@ -116,6 +116,7 @@
 #include <asm/mmu_context.h>
 
 #include <asm/suspend.h>
+#include <asm/kaiser.h>
 
 /*
  * max_low_pfn_mapped: highest direct mapped pfn under 4GB
@@ -1036,6 +1037,12 @@ void __init setup_arch(char **cmdline_p)
 	 * needs to be done after dmi_scan_machine, for the BP.
 	 */
 	init_hypervisor_platform();
+
+	/*
+	 * This needs to happen right after XENPV is set on xen and
+	 * kaiser_enabled is checked below in cleanup_highmap().
+	 */
+	kaiser_check_boottime_disable();
 
 	x86_init.resources.probe_roms();
 
