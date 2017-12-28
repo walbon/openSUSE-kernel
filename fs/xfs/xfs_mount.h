3b8863205d9a930c102206da8edac85c470ebf7e
Luis R. Rodriguez <mcgrof@suse.com>
2017-12-28 11:28:25 -0800

    xfs: add configurable error support to metadata buffers (bsc#1068569).
    suse-commit: 038b2e9476cf391e9293b9a67feef35649be3806

--- fs/xfs/xfs_mount.h
+++ fs/xfs/xfs_mount.h
@@ -372,5 +372,8 @@ extern struct xfs_dmops xfs_dmcore_xfs;
 
 int	xfs_zero_extent(struct xfs_inode *ip, xfs_fsblock_t start_fsb,
 			xfs_off_t count_fsb);
+struct xfs_error_cfg * xfs_error_get_cfg(struct xfs_mount *mp,
+		int error_class, int error);
+
 
 #endif	/* __XFS_MOUNT_H__ */
