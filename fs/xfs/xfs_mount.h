89248b6f475d58a453ebcba08644992bc2536610
Luis R. Rodriguez <mcgrof@suse.com>
2017-12-28 11:28:25 -0800

    xfs: add "fail at unmount" error handling configuration (bsc#1068569).
    suse-commit: eba69e007170637d6769095af343c5e4f9acc3a9

--- fs/xfs/xfs_mount.h
+++ fs/xfs/xfs_mount.h
@@ -179,6 +179,8 @@ typedef struct xfs_mount {
 	 * to various other kinds of pain inflicted on the pNFS server.
 	 */
 	__uint32_t		m_generation;
+
+	bool			m_fail_unmount;
 	const char		*m_mtpt;
 } xfs_mount_t;
 
@@ -188,6 +190,7 @@ typedef struct xfs_mount {
 #define XFS_MOUNT_WSYNC		(1ULL << 0)	/* for nfs - all metadata ops
 						   must be synchronous except
 						   for space allocations */
+#define XFS_MOUNT_UNMOUNTING	(1ULL << 1)	/* filesystem is unmounting */
 #define XFS_MOUNT_DMAPI		(1ULL << 2)	/* dmapi is enabled */
 #define XFS_MOUNT_WAS_CLEAN	(1ULL << 3)
 #define XFS_MOUNT_FS_SHUTDOWN	(1ULL << 4)	/* atomic stop of all filesystem
