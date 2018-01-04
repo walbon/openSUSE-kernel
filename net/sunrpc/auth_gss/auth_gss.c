377ce31150579378f37ff81c5756f17505765ffb
NeilBrown <neilb@suse.com>
2018-01-04 13:54:10 +1100

    sunrpc: add RPCSEC_GSS hash_cred() function (bsc#1012917).
    suse-commit: f387bd62d689c0a30a8f43d37c90ee7fe4f010c7

--- net/sunrpc/auth_gss/auth_gss.c
+++ net/sunrpc/auth_gss/auth_gss.c
@@ -1300,6 +1300,13 @@ gss_destroy_cred(struct rpc_cred *cred)
 	gss_destroy_nullcred(cred);
 }
 
+static int
+gss_hash_cred(struct auth_cred *acred, unsigned int hashbits)
+{
+	u64 uid = from_kuid(&init_user_ns, acred->uid);
+	return hash_64(uid, hashbits);
+}
+
 /*
  * Lookup RPCSEC_GSS cred for the current process
  */
@@ -1984,6 +1991,7 @@ static const struct rpc_authops authgss_ops = {
 	.au_name	= "RPCSEC_GSS",
 	.create		= gss_create,
 	.destroy	= gss_destroy,
+	.hash_cred	= gss_hash_cred,
 	.lookup_cred	= gss_lookup_cred,
 	.crcreate	= gss_create_cred,
 	.list_pseudoflavors = gss_mech_list_pseudoflavors,
