4162e5da7ca0ce99c78ad9c36dabca178f80b901
Mian Yousaf Kaukab <yousaf.kaukab@suse.com>
2018-01-15 11:16:51 +0100

    arm64: entry.S: Remove disable_dbg (bsc#1068032).
    suse-commit: 34907ac77f009d34cc6ce7d0a818c242293eaaff

--- arch/arm64/include/asm/assembler.h
+++ arch/arm64/include/asm/assembler.h
@@ -79,13 +79,6 @@
 	msr	daif, \flags
 	.endm
 
-/*
- * Enable and disable debug exceptions.
- */
-	.macro	disable_dbg
-	msr	daifset, #8
-	.endm
-
 	.macro	enable_dbg
 	msr	daifclr, #8
 	.endm
@@ -99,9 +92,9 @@
 9990:
 	.endm
 
+	/* call with daif masked */
 	.macro	enable_step_tsk, flgs, tmp
 	tbz	\flgs, #TIF_SINGLESTEP, 9990f
-	disable_dbg
 	mrs	\tmp, mdscr_el1
 	orr	\tmp, \tmp, #1
 	msr	mdscr_el1, \tmp
