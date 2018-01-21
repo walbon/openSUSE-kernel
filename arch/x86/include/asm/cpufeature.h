43b010eedab6eb0e0418aab393b0f28762fcc21e
Jiri Slaby <jslaby@suse.cz>
2018-01-21 22:22:31 +0100

    - x86/retpoline: Fill RSB on context switch for affected CPUs   (bsc#1068032 CVE-2017-5715). - Refresh   patches.suse/01-x86-feature-enable-the-x86-feature-to-control-speculation.patch. - Refresh patches.suse/stack-unwind.patch.
    suse-commit: 299bd08bd8c5f526b54a9b735506b81ad9944a71

--- arch/x86/include/asm/cpufeature.h
+++ arch/x86/include/asm/cpufeature.h
@@ -203,7 +203,8 @@
 #define X86_FEATURE_INTEL_PT	( 7*32+15) /* Intel Processor Trace */
 #define X86_FEATURE_AVX512_4VNNIW (7*32+16) /* AVX-512 Neural Network Instructions */
 #define X86_FEATURE_AVX512_4FMAPS (7*32+17) /* AVX-512 Multiply Accumulation Single precision */
-#define X86_FEATURE_SPEC_CTRL	( 7*32+19) /* Control Speculation Control */
+#define X86_FEATURE_RSB_CTXSW	( 7*32+19) /* Fill RSB on context switches */
+#define X86_FEATURE_SPEC_CTRL	( 7*32+20) /* Control Speculation Control */
 
 #define X86_FEATURE_RETPOLINE	( 7*32+29) /* Generic Retpoline mitigation for Spectre variant 2 */
 #define X86_FEATURE_RETPOLINE_AMD ( 7*32+30) /* AMD Retpoline mitigation for Spectre variant 2 */
