624e15f48f1450a702c2a2d276e3dfcbbd885dba
Hannes Reinecke <hare@suse.de>
2018-02-14 09:25:09 +0100

    scsi: qla2xxx: Add debug knob for user control workload (bsc#1043725,FATE#324770).
    suse-commit: 846a53a5a6f1b38bf21d6bcbe6badfad66582de1

--- drivers/scsi/qla2xxx/qla_def.h
+++ drivers/scsi/qla2xxx/qla_def.h
@@ -3356,6 +3356,7 @@ struct qlt_hw_data {
 
 	struct dentry *dfs_tgt_sess;
 	struct dentry *dfs_tgt_port_database;
+	struct dentry *dfs_naqp;
 
 	struct list_head q_full_list;
 	uint32_t num_pend_cmds;
@@ -3364,7 +3365,8 @@ struct qlt_hw_data {
 	spinlock_t q_full_lock;
 	uint32_t leak_exchg_thresh_hold;
 	spinlock_t sess_lock;
-	int rspq_vector_cpuid;
+	int num_act_qpairs;
+#define DEFAULT_NAQP 2
 	spinlock_t atio_lock ____cacheline_aligned;
 	struct btree_head32 host_map;
 };
@@ -4313,6 +4315,9 @@ enum nexus_wait_type {
 	WAIT_TARGET,
 	WAIT_LUN,
 };
+
+#define USER_CTRL_IRQ(_ha) (ql2xuctrlirq && QLA_TGT_MODE_ENABLED() && \
+	(IS_QLA27XX(_ha) || IS_QLA83XX(_ha)))
 
 #include "qla_target.h"
 #include "qla_gbl.h"
