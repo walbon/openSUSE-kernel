41a5485497f7ea92a94dc3891b59d30b3d8a087e
Borislav Petkov <bp@suse.de>
2018-02-28 18:34:23 +0100

    x86/mce: Pin the timer when modifying (bsc#1080851,1076282).
    suse-commit: 4655d8ce95912caa65088db60debfc54c254ca88

--- arch/x86/kernel/cpu/mcheck/mce.c
+++ arch/x86/kernel/cpu/mcheck/mce.c
@@ -1345,7 +1345,7 @@ static void __start_timer(struct timer_list *t, unsigned long interval)
 	local_irq_save(flags);
 
 	if (!timer_pending(t) || time_before(when, t->expires))
-		mod_timer(t, round_jiffies(when));
+		mod_timer_pinned(t, round_jiffies(when));
 
 	local_irq_restore(flags);
 }
